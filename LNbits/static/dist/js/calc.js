2e4b26664864a941b492b44e16f94c915e4513e6
Arc <33088785+arcbtc@users.noreply.github.com>
2019-12-12 23:11:30 +0000

    Add files via upload

---
+++ LNbits/static/dist/js/calc.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    $('.num').click(function () {
+        var num = $(this);
+        var text = $.trim(num.find('.txt').clone().children().remove().end().text());
+        var telNumber = $('#telNumber');
+        $(telNumber).val(telNumber.val() + text);
+    });
+
+    });
