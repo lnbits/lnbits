f7651634868f04fe6bb24f58e42509f5af3dea28
Arc <33088785+arcbtc@users.noreply.github.com>
2020-01-15 15:16:10 +0000

    Updated requests

--- lnbits/wallets/opennode.py
+++ lnbits/wallets/opennode.py
@@ -1,17 +1,15 @@
 from requests import Response, get, post
-
+from flask import jsonify
 from .base import InvoiceResponse, TxStatus, Wallet
+import json
 
-
-class OpennodeWallet(Wallet):
+class OpenNodeWallet(Wallet):
     """https://api.lightning.community/rest/index.html#lnd-rest-api-reference"""
 
     def __init__(self, *, endpoint: str, admin_key: str, invoice_key: str):
         self.endpoint = endpoint[:-1] if endpoint.endswith("/") else endpoint
         self.auth_admin = {"Authorization": admin_key}
         self.auth_invoice = {"Authorization": invoice_key}
-
-
 
     def create_invoice(self, amount: int, memo: str = "") -> InvoiceResponse:
         payment_hash, payment_request = None, None
@@ -22,54 +20,40 @@ class OpennodeWallet(Wallet):
         )
         if r.ok:
             data = r.json()
-            payment_hash, payment_request = data["data"]["id"], data["data"]["lightning_invoice"]["payreq"]
+            payment_hash, payment_request = data['data']['id'], data["data"]["lightning_invoice"]["payreq"]
 
         return InvoiceResponse(r, payment_hash, payment_request)
 
-    def get_invoice_status(self, payment_hash: str) -> TxStatus:
-        print(f"{self.endpoint}/v1/charge/{payment_hash}")
-        print(self.auth_invoice)
-        r = get(url=f"{self.endpoint}/v1/charge/{payment_hash}", headers=self.auth_invoice)
-        data = r.json()
-        
-        if r.ok:
-            data = r.json()
-            if data["data"]["status"] != "paid":
-                return TxStatus(r, None)
-            else:
-                return TxStatus(r, True)
-
-        else:
-            return TxStatus(r, False)
-
-        
-
     def pay_invoice(self, bolt11: str) -> Response:
-        payment_hash = None
-        r = post(
-            url=f"{self.endpoint}/v2/withdrawals",
-            headers=self.auth_admin,
-            json={"type": "ln", "address": bolt11},
+        return post(
+            url=f"{self.endpoint}/v2/withdrawals", headers=self.auth_admin, json={"type": "ln", "address": bolt11}
         )
 
-        if r.ok:
-            data = r.json()
-            payment_hash = data["data"]["id"]
 
-        return (r, payment_hash)
+
+    def get_invoice_status(self, payment_hash: str) -> TxStatus:
+
+        r = get(url=f"{self.endpoint}/v1/charge/{payment_hash}", headers=self.auth_invoice)
+
+        data = r.json()
+        print(data)
+        print(f"{self.endpoint}/v1/charge/{payment_hash} {self.auth_invoice}")
+        if not r.ok:
+            return TxStatus(r, None)
+        
+        
+        statuses = {"processing": None, "paid": True, "unpaid": False}
+        return TxStatus(r, statuses[r.json()["data"]["status"]])
 
     def get_payment_status(self, payment_hash: str) -> TxStatus:
+
         r = get(url=f"{self.endpoint}/v1/withdrawal/{payment_hash}", headers=self.auth_admin)
 
-        print(f"{self.endpoint}/v1/withdrawal/{payment_hash}")
+        if not r.ok:
+            return TxStatus(r, None)
 
-        if r.ok:
-            data = r.json()
-            if ["data"]["status"] != "confirmed":
-                return TxStatus(r, None)
-            else:
-                return TxStatus(r, True)
-        else:
-            return TxStatus(r, False)
+        statuses = {"pending": None, "confirmed": True, "error": False, "failed": False}
+        return TxStatus(r, statuses[r.json()["data"]["status"]])
+       
 
         
