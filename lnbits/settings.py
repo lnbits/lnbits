8f20755fd67ca6525085ed437304c7ae296406db
Arc <33088785+arcbtc@users.noreply.github.com>
2020-01-15 15:19:54 +0000

    Update settings.py

--- lnbits/settings.py
+++ lnbits/settings.py
@@ -1,17 +1,13 @@
 import os
 
-from .wallets import LntxbotWallet  # OR LndWallet OR OpennodeWallet
-
-
-WALLET = LntxbotWallet(
-    endpoint=os.getenv("LNTXBOT_API_ENDPOINT"),
-    admin_key=os.getenv("LNTXBOT_ADMIN_KEY"),
-    invoice_key=os.getenv("LNTXBOT_INVOICE_KEY"),
-)
-
+from .wallets import OpenNodeWallet  # OR LndWallet OR OpennodeWallet
+ 
+WALLET = OpenNodeWallet(endpoint=os.getenv("OPENNODE_API_ENDPOINT"),admin_key=os.getenv("OPENNODE_ADMIN_KEY"),invoice_key=os.getenv("OPENNODE_INVOICE_KEY"))
 # OR
+# WALLET = LntxbotWallet(endpoint=os.getenv("LNTXBOT_API_ENDPOINT"),admin_key=os.getenv("LNTXBOT_ADMIN_KEY"),invoice_key=os.getenv("LNTXBOT_INVOICE_KEY"),)
 # WALLET = LndWallet(endpoint=os.getenv("LND_API_ENDPOINT"), admin_macaroon=os.getenv("LND_ADMIN_MACAROON"))
-# WALLET = OpennodeWallet(endpoint=os.getenv("OPENNODE_API_ENDPOINT"),admin_key=os.getenv("OPENNODE_ADMIN_KEY"),invoice_key=os.getenv("OPENNODE_INVOICE_KEY"))
+
+
 
 LNBITS_PATH = os.path.dirname(os.path.realpath(__file__))
 DATABASE_PATH = os.getenv("DATABASE_PATH") or os.path.join(LNBITS_PATH, "data", "database.sqlite3")
