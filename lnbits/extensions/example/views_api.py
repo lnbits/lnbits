3fb570c9fa875c3698fe7a5a9d13c20782064aaa
Arc <33088785+arcbtc@users.noreply.github.com>
2020-02-19 23:21:01 +0000

    Create views_api.py

---
+++ lnbits/extensions/example/views_api.py
@@ -0,0 +1,17 @@
+#views_api.py is for you API endpoints that could be hit by another service
+
+#add your dependencies here
+
+from flask import jsonify, render_template, request, redirect, url_for
+from lnbits.db import open_db, open_ext_db
+from lnbits.extensions.example import example_ext
+
+#add your endpoints here
+
+@example_ext.route("/api/v1/example", methods=["GET","POST"])
+def api_example():
+    """Try to add descriptions for others."""
+    #YOUR-CODE
+    
+    return jsonify({"status": "TRUE"), 200
+    
