f364ce51c04d3c892bdebd23e0a9c151beae6ffb
Arc <33088785+arcbtc@users.noreply.github.com>
2020-04-02 07:44:35 +0100

    Update __init__.py

--- lnbits/wallets/__init__.py
+++ lnbits/wallets/__init__.py
@@ -1,6 +1,7 @@
 # flake8: noqa
 
 from .lnd import LndWallet
+from .lndgrpc import LndWalletgrpc
 from .lntxbot import LntxbotWallet
 from .opennode import OpenNodeWallet
 from .lnpay import LNPayWallet
