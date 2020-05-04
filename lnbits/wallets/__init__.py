9eb253fe5584197eea7fdcb563a76767e22e02e0
Eneko Illarramendi <eneko@illarra.com>
2020-05-04 17:34:53 +0200

    fix: typo

--- lnbits/wallets/__init__.py
+++ lnbits/wallets/__init__.py
@@ -5,5 +5,5 @@ from .lndgrpc import LndWallet
 from .lntxbot import LntxbotWallet
 from .opennode import OpenNodeWallet
 from .lnpay import LNPayWallet
-from .lnbits import LnbitsWallet
+from .lnbits import LNbitsWallet
 from .lndrest import LndRestWallet
