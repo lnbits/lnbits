75047c5bc350fd3271ee7867c5bfc795a07cecef
fiatjaf <fiatjaf@alhur.es>
2020-09-16 20:21:42 -0300

    fix static extensions js url.

--- lnbits/extensions/withdraw/__init__.py
+++ lnbits/extensions/withdraw/__init__.py
@@ -1,9 +1,7 @@
 from quart import Blueprint
 
 
-withdraw_ext: Blueprint = Blueprint(
-    "withdraw", __name__, static_folder="static", template_folder="templates", static_url_path="/static"
-)
+withdraw_ext: Blueprint = Blueprint("withdraw", __name__, static_folder="static", template_folder="templates")
 
 
 from .views_api import *  # noqa
