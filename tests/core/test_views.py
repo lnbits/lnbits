cd8a447ba041657c20323ac27a9aa7c5883c6c95
fiatjaf <fiatjaf@alhur.es>
2020-09-17 22:41:30 -0300

    pytest quibbles and dev dependencies.

--- tests/core/test_views.py
+++ tests/core/test_views.py
@@ -1,3 +1,7 @@
-def test_homepage(client):
-    r = client.get("/")
-    assert b"Add a new wallet" in r.data
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_homepage(client):
+    r = await client.get("/")
+    assert b"Add a new wallet" in await r.get_data()
