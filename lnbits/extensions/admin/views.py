d5d92ef1c2ae1e860e74cb18fc023896c212410c
benarc <ben@arc.wales>
2020-09-30 20:34:32 +0100

    Extension updates admin tables

--- lnbits/extensions/admin/views.py
+++ lnbits/extensions/admin/views.py
@@ -11,13 +11,20 @@ from lnbits.settings import WALLET
 @check_user_exists()
 def index():
     user_id = request.args.get("usr", type=str)
+    print(user_id)
     admin = get_admin()
-    if admin.user != user_id:
-        abort(HTTPStatus.FORBIDDEN, "Admin only")
+    if admin != None:
+        if admin[0] == None:
+            admin_user = get_user(create_account().id).id
+        if admin.user != user_id:
+            abort(HTTPStatus.FORBIDDEN, "Admin only")
+        if not user_id:
+            admin_user = get_user(create_account().id).id
+            print(admin_user)
+            admin = get_admin()
+        else:
+            admin_user = user_id
+            
     funding = get_funding()
-    if admin[0] != None:
-        admin_user = admin[0]
-    if admin.user != None and admin.user != user_id:
-        abort(HTTPStatus.FORBIDDEN, "Admin only")
 
     return render_template("admin/index.html", user=g.user, admin=admin, funding=funding)
