2cce787f1deca52b54c99cda9c24cc5d3dc7fb12
grmkris <kristjan.grm1@gmail.com>
2021-02-07 17:05:04 +0100

    added inital models.py and index.html for the extension

---
+++ lnbits/extensions/challonge/models.py
@@ -0,0 +1,26 @@
+from typing import NamedTuple
+
+class Tournament(NamedTuple):
+    id: str
+    wallet: str
+    challonge_API: str
+    challonge_tournament_id: str
+    challonge_tournament_name: str
+    signup_fee: int 
+    prize_pool: int
+    total_prize_pool: int
+    max_participants: int
+    current_participants: int
+    status: str # STARTED, COMPLETED, COMPLETED_PAID, CANCELED
+    winner_id: str
+    webhook: str
+    time: int # ??? timestamp
+    start_time: int
+
+class Participant(NamedTuple):
+    id: str
+    tournament: str
+    secret: str
+    status: str # (SIGNUP, PAID)
+    username: str
+    challonge_username: str
