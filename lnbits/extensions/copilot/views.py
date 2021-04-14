6dceb61ba81834fedffe64588c74a84a553633ba
Ben Arc <ben@arc.wales>
2021-04-14 17:00:18 +0100

    Very hacky checker for websockets

--- lnbits/extensions/copilot/views.py
+++ lnbits/extensions/copilot/views.py
@@ -6,11 +6,25 @@ from lnbits.decorators import check_user_exists, validate_uuids
 from . import copilot_ext
 from .crud import get_copilot
 
-@copilot_ext.websocket('/ws')
-async def ws():
+from quart import g, abort, render_template, jsonify, websocket
+from functools import wraps
+import trio 
+import shortuuid
+connected_websockets = {}
+
+@copilot_ext.websocket('/ws/panel/<copilot_id>')
+async def ws_panel(copilot_id):
+    global connected_websockets
     while True:
         data = await websocket.receive()
-        await websocket.send(f"echo {data}")
+        connected_websockets[copilot_id] = shortuuid.uuid() + '-' + data
+
+@copilot_ext.websocket('/ws/compose/<copilot_id>')
+async def ws_compose(copilot_id):
+    global connected_websockets
+    while True:
+        data = await websocket.receive()
+        await websocket.send(connected_websockets[copilot_id])
         
 @copilot_ext.route("/")
 @validate_uuids(["usr"], required=True)
