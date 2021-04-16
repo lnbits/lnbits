f8603ed6434a0c516481866faffe1c1169ab04e5
Ben Arc <ben@arc.wales>
2021-04-16 21:16:15 +0100

    metadata change

--- lnbits/extensions/copilot/lnurl.py
+++ lnbits/extensions/copilot/lnurl.py
@@ -20,7 +20,7 @@ async def lnurl_response(cp_id):
         callback=url_for("copilot.lnurl_callback", cp_id=cp_id, _external=True),
         min_sendable=10000,
         max_sendable=50000000,
-        metadata=LnurlPayMetadata(json.dumps([["text/plain", cp.lnurl_title]])),
+        metadata="{'title':'" + cp.lnurl_title + "'}",
     )
 
     params = resp.dict()
