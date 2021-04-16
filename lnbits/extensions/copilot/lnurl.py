88e11b4793fe7a854f971dc97c1e98cc088d5517
Ben Arc <ben@arc.wales>
2021-04-16 22:02:22 +0100

    dasv

--- lnbits/extensions/copilot/lnurl.py
+++ lnbits/extensions/copilot/lnurl.py
@@ -67,7 +67,7 @@ async def lnurl_callback(cp_id):
         wallet_id=cp.wallet,
         amount=int(amount_received / 1000),
         memo=cp.lnurl_title,
-        webhook=url_for("copilot.lnurl_callback", copilot=cp_id, amount=int(amount_received / 1000), _external=True),
+        webhook=url_for("copilot.api_copilot_hooker", copilot_id=cp_id, amount=int(amount_received / 1000), _external=False),
         description_hash=hashlib.sha256((LnurlPayMetadata(json.dumps([["text/plain", str(cp.lnurl_title)]]))).encode("utf-8")).digest(),
         extra={"tag": "copilot", "comment": comment},
     )
