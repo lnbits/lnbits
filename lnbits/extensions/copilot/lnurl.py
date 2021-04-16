79b046ede17675d96d5ce13262b2e97ed32c559e
Ben Arc <ben@arc.wales>
2021-04-16 12:22:22 +0100

    json fix

--- lnbits/extensions/copilot/lnurl.py
+++ lnbits/extensions/copilot/lnurl.py
@@ -73,7 +73,7 @@ async def lnurl_callback(cp_id):
 
     resp = LnurlPayActionResponse(
         pr=payment_request,
-        success_action=jsonify({tag: "message", message: "Thank you!"}),
+        success_action=jsonify({"tag": "message", "message": "Thank you!"}),
         routes=[],
     )
     print(payment_request)
