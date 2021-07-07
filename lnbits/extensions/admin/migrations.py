02ee5e605138a0e310c355a6a7bb73ced38f79a7
Tiago vasconcelos <talvasconcelos@gmail.com>
2021-07-07 16:42:14 +0100

    fixed names on envs and added selected wallet

--- lnbits/extensions/admin/migrations.py
+++ lnbits/extensions/admin/migrations.py
@@ -86,8 +86,9 @@ async def m001_create_admin_table(db):
 
 async def m001_create_funding_table(db):
 
-    # Make the funding table,  if it does not already exist
+    funding_wallet = getenv("LNBITS_BACKEND_WALLET_CLASS")
 
+    # Make the funding table,  if it does not already exist
     await db.execute(
         """
         CREATE TABLE IF NOT EXISTS funding (
@@ -99,7 +100,8 @@ async def m001_create_funding_table(db):
             invoice_key TEXT,
             admin_key TEXT,
             cert TEXT,
-            balance int
+            balance INT,
+            selected INT
         );
     """
     )
@@ -133,112 +135,130 @@ async def m001_create_funding_table(db):
 
     await db.execute(
         """
-        INSERT INTO funding (id, backend_wallet, endpoint)
-        VALUES (?, ?, ?)
+        INSERT INTO funding (id, backend_wallet, endpoint, selected)
+        VALUES (?, ?, ?, ?)
         """,
-        (urlsafe_short_hash(), "CLightningWallet", getenv("CLIGHTNING_RPC")),
+        (
+            urlsafe_short_hash(),
+            "CLightningWallet",
+            getenv("CLIGHTNING_RPC"),
+            1 if funding_wallet == "CLightningWallet" else 0,
+        ),
     )
 
     await db.execute(
         """
-        INSERT INTO funding (id, backend_wallet, endpoint, admin_key)
-        VALUES (?, ?, ?, ?)
+        INSERT INTO funding (id, backend_wallet, endpoint, admin_key, selected)
+        VALUES (?, ?, ?, ?, ?)
         """,
         (
             urlsafe_short_hash(),
             "LnbitsWallet",
             getenv("LNBITS_ENDPOINT"),
             getenv("LNBITS_KEY"),
+            1 if funding_wallet == "LnbitsWallet" else 0,
         ),
     )
 
     await db.execute(
         """
-        INSERT INTO funding (id, backend_wallet, endpoint, port, read_key, invoice_key, admin_key, cert)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        INSERT INTO funding (id, backend_wallet, endpoint, port, admin_key, cert, selected)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
         """,
         (
             urlsafe_short_hash(),
             "LndWallet",
             getenv("LND_GRPC_ENDPOINT"),
             getenv("LND_GRPC_PORT"),
-            getenv("LND_READ_MACAROON"),
-            getenv("LND_INVOICE_MACAROON"),
-            getenv("LND_ADMIN_MACAROON"),
-            getenv("LND_CERT"),
+            getenv("LND_GRPC_MACAROON"),
+            getenv("LND_GRPC_CERT"),
+            1 if funding_wallet == "LndWallet" else 0,
         ),
     )
 
     await db.execute(
         """
-        INSERT INTO funding (id, backend_wallet, endpoint, read_key, invoice_key, admin_key, cert)
-        VALUES (?, ?, ?, ?, ?, ?, ?)
+        INSERT INTO funding (id, backend_wallet, endpoint, admin_key, cert, selected)
+        VALUES (?, ?, ?, ?, ?, ?)
         """,
         (
             urlsafe_short_hash(),
             "LndRestWallet",
             getenv("LND_REST_ENDPOINT"),
-            getenv("LND_REST_READ_MACAROON"),
-            getenv("LND_REST_INVOICE_MACAROON"),
-            getenv("LND_REST_ADMIN_MACAROON"),
+            getenv("LND_REST_MACAROON"),
             getenv("LND_REST_CERT"),
+            1 if funding_wallet == "LndWallet" else 0,
         ),
     )
 
     await db.execute(
         """
-        INSERT INTO funding (id, backend_wallet, endpoint, read_key, invoice_key, admin_key, cert)
-        VALUES (?, ?, ?, ?, ?, ?, ?)
+        INSERT INTO funding (id, backend_wallet, endpoint, admin_key, cert, selected)
+        VALUES (?, ?, ?, ?, ?, ?)
         """,
         (
             urlsafe_short_hash(),
             "LNPayWallet",
             getenv("LNPAY_API_ENDPOINT"),
-            getenv("LNPAY_READ_KEY"),
-            getenv("LNPAY_INVOICE_KEY"),
             getenv("LNPAY_ADMIN_KEY"),
-            getenv("LNPAY_API_KEY"),
+            getenv("LNPAY_API_KEY"),  # this is going in as the cert
+            1 if funding_wallet == "LNPayWallet" else 0,
         ),
     )
 
     await db.execute(
         """
-        INSERT INTO funding (id, backend_wallet, endpoint, invoice_key, admin_key)
+        INSERT INTO funding (id, backend_wallet, endpoint, admin_key, selected)
         VALUES (?, ?, ?, ?, ?)
         """,
         (
             urlsafe_short_hash(),
             "LntxbotWallet",
             getenv("LNTXBOT_API_ENDPOINT"),
-            getenv("LNTXBOT_INVOICE_KEY"),
-            getenv("LNTXBOT_ADMIN_KEY"),
+            getenv("LNTXBOT_KEY"),
+            1 if funding_wallet == "LntxbotWallet" else 0,
         ),
     )
 
     await db.execute(
         """
-        INSERT INTO funding (id, backend_wallet, endpoint, invoice_key, admin_key)
+        INSERT INTO funding (id, backend_wallet, endpoint, admin_key, selected)
         VALUES (?, ?, ?, ?, ?)
         """,
         (
             urlsafe_short_hash(),
             "OpenNodeWallet",
             getenv("OPENNODE_API_ENDPOINT"),
-            getenv("OPENNODE_INVOICE_KEY"),
-            getenv("OPENNODE_ADMIN_KEY"),
+            getenv("OPENNODE_KEY"),
+            1 if funding_wallet == "OpenNodeWallet" else 0,
         ),
     )
 
     await db.execute(
         """
-        INSERT INTO funding (id, backend_wallet, endpoint, invoice_key, admin_key)
+        INSERT INTO funding (id, backend_wallet, endpoint, admin_key, selected)
         VALUES (?, ?, ?, ?, ?)
         """,
         (
             urlsafe_short_hash(),
             "SparkWallet",
             getenv("SPARK_URL"),
-            getenv("SPARK_INVOICE_KEY"),  # doesn't exist
             getenv("SPARK_TOKEN"),
+            1 if funding_wallet == "SparkWallet" else 0,
         ),
     )
+
+    ## PLACEHOLDER FOR ECLAIR WALLET
+    # await db.execute(
+    #     """
+    #     INSERT INTO funding (id, backend_wallet, endpoint, admin_key, selected)
+    #     VALUES (?, ?, ?, ?, ?)
+    #     """,
+    #     (
+    #         urlsafe_short_hash(),
+    #         "EclairWallet",
+    #         getenv("ECLAIR_URL"),
+    #         getenv("ECLAIR_PASS"),
+    #         1 if funding_wallet == "EclairWallet" else 0,
+    #     ),
+    # )
