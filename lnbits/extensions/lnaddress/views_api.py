bf6308e1305f3cfec1d02efa284022451fede35a
Tiago vasconcelos <talvasconcelos@gmail.com>
2021-09-07 16:21:31 +0100

    remove print

--- lnbits/extensions/lnaddress/views_api.py
+++ lnbits/extensions/lnaddress/views_api.py
@@ -31,7 +31,7 @@ async def api_domains():
 
     if "all_wallets" in request.args:
         wallet_ids = (await get_user(g.wallet.user)).wallet_ids
-    print(urlparse(request.url_root).netloc)
+    
     return (
         jsonify([domain._asdict() for domain in await get_domains(wallet_ids)]),
         HTTPStatus.OK,
