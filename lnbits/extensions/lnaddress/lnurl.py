587444a9995262234d355c3d54eb50b8b50f3501
Tiago vasconcelos <talvasconcelos@gmail.com>
2021-09-08 10:18:01 +0100

    await missed

--- lnbits/extensions/lnaddress/lnurl.py
+++ lnbits/extensions/lnaddress/lnurl.py
@@ -31,7 +31,7 @@ async def lnurl_response(username: str, domain: str):
 
 @lnaddress_ext.route("/lnurl/cb/<address_id>", methods=["GET"])
 async def lnurl_callback(address_id):
-    address = get_address(address_id)
+    address = await get_address(address_id)
 
     if not address:
         return jsonify({"status": "ERROR", "reason": "Couldn't find username."})
@@ -53,7 +53,7 @@ async def lnurl_callback(address_id):
             ).dict()
         )
 
-    domain = get_domain(address.domain)
+    domain = await get_domain(address.domain)
 
     async with httpx.AsyncClient() as client:
         try:
