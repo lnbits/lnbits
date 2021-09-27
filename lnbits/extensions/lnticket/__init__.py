3a3ef4cd2217a3ede94e776323fb2295b9da827d
Tiago vasconcelos <talvasconcelos@gmail.com>
2021-09-27 20:33:18 +0100

    tickets fastAPI: init

--- lnbits/extensions/lnticket/__init__.py
+++ lnbits/extensions/lnticket/__init__.py
@@ -1,11 +1,24 @@
-from quart import Blueprint
+from fastapi import APIRouter, FastAPI
+from fastapi.staticfiles import StaticFiles
+from starlette.routing import Mount
+
 from lnbits.db import Database
+from lnbits.helpers import template_renderer
 
 db = Database("ext_lnticket")
 
-lnticket_ext: Blueprint = Blueprint(
-    "lnticket", __name__, static_folder="static", template_folder="templates"
+lnticket_ext: APIRouter = APIRouter(
+    prefix="/lnticket",
+    tags=["LNTicket"]
+    # "lnticket", __name__, static_folder="static", template_folder="templates"
 )
+
+def lnticket_renderer():
+    return template_renderer(
+        [
+            "lnbits/extensions/lnticket/templates",
+        ]
+    )
 
 
 from .views_api import *  # noqa
