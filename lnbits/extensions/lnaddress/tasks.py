f9fb85cbbfa4fd071d43dd895124f89448a8d13b
Tiago vasconcelos <talvasconcelos@gmail.com>
2021-10-07 16:00:20 +0100

    added webhook functionality

--- lnbits/extensions/lnaddress/tasks.py
+++ lnbits/extensions/lnaddress/tasks.py
@@ -3,7 +3,7 @@ from quart.json import jsonify
 import trio
 import httpx
 
-from .crud import get_domain, set_address_paid, set_address_renewed
+from .crud import get_domain, set_address_paid, set_address_renewed, get_address
 from lnbits.core.crud import get_user, get_wallet
 from lnbits.core import db as core_db
 from lnbits.core.models import Payment
@@ -28,7 +28,7 @@ async def on_invoice_paid(payment: Payment) -> None:
         await set_address_paid(payment_hash=payment.payment_hash)
 
     elif "renew lnaddress" == payment.extra.get("tag"):
-        
+
         await payment.set_pending(False)
         await set_address_renewed(address_id=payment.extra["id"], duration=payment.extra["duration"])
 
@@ -37,22 +37,21 @@ async def on_invoice_paid(payment: Payment) -> None:
 
 
     ### Use webhook to notify about cloudflare registration
-    # if domain.webhook:
-    #     async with httpx.AsyncClient() as client:
-    #         try:
-    #             r = await client.post(
-    #                 domain.webhook,
-    #                 json={
-    #                     "domain": address.domain_name,
-    #                     "address": subdomain.subdomain,
-    #                     "record_type": subdomain.record_type,
-    #                     "email": subdomain.email,
-    #                     "ip": subdomain.ip,
-    #                     "cost:": str(subdomain.sats) + " sats",
-    #                     "duration": str(subdomain.duration) + " days",
-    #                     "cf_response": cf_response,
-    #                 },
-    #                 timeout=40,
-    #             )
-    #         except AssertionError:
-    #             webhook = None
+    if domain.webhook:
+        address = await get_address(payment.payment_hash)
+        domain = await get_domain(address.domain)
+        async with httpx.AsyncClient() as client:
+            try:
+                r = await client.post(
+                    domain.webhook,
+                    json={
+                        "domain": domain.domain,
+                        "address": address.username,
+                        "email": address.email || "",
+                        "cost:": str(address.sats) + " sats",
+                        "duration": str(address.duration) + " days",
+                    },
+                    timeout=40,
+                )
+            except AssertionError:
+                webhook = None
