0b9adcf8ad334ba1b2957844606bebe4ad7943f6
Joe Pentland <joe.pentland@gmail.com>
2021-10-07 18:33:16 +0200

    base: Refresh local currency rate every 10 minutes

--- lnbits/static/js/base.js
+++ lnbits/static/js/base.js
@@ -399,6 +399,9 @@ window.windowMixin = {
     if (currency) {
       this.setLocalCurrency(currency)
     }
+    setInterval(() => {
+      this.setLocalCurrency(this.g.localCurrency)
+    }, 600000)
 
     if (window.extensions) {
       var user = this.g.user
