c1a82dad9d1ecf8b97d73fabf543a48e8a5cbe14
Stefan Stammberger <some.fusion@gmail.com>
2021-10-10 11:05:43 +0200

    fix: lnurlp templates and path functions
    While rendering the tempaltes the link.lnurl() function was not found
    as no request object was passed.
    
    Compute the lnurl before rendering the template. This way we can avoid
    computing it multiple times and do not to have to pass the request object
    in the template.

--- lnbits/extensions/lnurlp/views.py
+++ lnbits/extensions/lnurlp/views.py
@@ -30,8 +30,8 @@ async def display(request: Request,link_id):
             detail="Pay link does not exist."
         )
         # abort(HTTPStatus.NOT_FOUND, "Pay link does not exist.")
-
-    return lnurlp_renderer().TemplateResponse("lnurlp/display.html", {"request": request, "link":link})
+    ctx = {"request": request, "lnurl":link.lnurl(req=request)}
+    return lnurlp_renderer().TemplateResponse("lnurlp/display.html", ctx)
 
 
 @lnurlp_ext.get("/print/{link_id}", response_class=HTMLResponse)
@@ -43,5 +43,5 @@ async def print_qr(request: Request,link_id):
             detail="Pay link does not exist."
         )
         # abort(HTTPStatus.NOT_FOUND, "Pay link does not exist.")
-
-    return lnurlp_renderer().TemplateResponse("lnurlp/print_qr.html", {"request": request, "link":link})
+    ctx = {"request": request, "lnurl":link.lnurl(req=request)}
+    return lnurlp_renderer().TemplateResponse("lnurlp/print_qr.html", ctx)
