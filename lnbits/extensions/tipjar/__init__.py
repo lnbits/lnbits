b97cef0e3749cd7490154e885a2361f632a11d69
Tiago vasconcelos <talvasconcelos@gmail.com>
2021-10-20 16:34:00 +0100

    init clean up

--- lnbits/extensions/tipjar/__init__.py
+++ lnbits/extensions/tipjar/__init__.py
@@ -1,10 +1,7 @@
-import asyncio
-from fastapi import APIRouter, FastAPI
-from fastapi.staticfiles import StaticFiles
-from starlette.routing import Mount
+from fastapi import APIRouter
+
 from lnbits.db import Database
 from lnbits.helpers import template_renderer
-from lnbits.tasks import catch_everything_and_restart
 
 db = Database("ext_tipjar")
 
@@ -15,5 +12,5 @@ def tipjar_renderer():
     return template_renderer(["lnbits/extensions/tipjar/templates"])
 
 
-from .views_api import *  # noqa
 from .views import *  # noqa
+from .views_api import *  # noqa
