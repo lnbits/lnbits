c734526bc1a9975f4f0bc90cd7938715aa0661c8
benarc <ben@arc.wales>
2021-11-03 10:12:14 +0000

    reverted exception

--- lnbits/core/views/api.py
+++ lnbits/core/views/api.py
@@ -329,9 +329,7 @@ async def api_payment(payment_hash):
     await check_invoice_status(payment.wallet_id, payment_hash)
     payment = await get_standalone_payment(payment_hash)
     if not payment:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail="Payment does not exist."
-        )
+        return {"message": "Payment does not exist."}
     elif not payment.pending:
         return {"paid": True, "preimage": payment.preimage}
 
