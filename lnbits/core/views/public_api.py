2b0bd43974428899b281423b6a3ff5907bd72ade
Tiago vasconcelos <talvasconcelos@gmail.com>
2021-11-25 17:26:46 +0000

    clean prints

--- lnbits/core/views/public_api.py
+++ lnbits/core/views/public_api.py
@@ -16,10 +16,7 @@ from ..tasks import api_invoice_listeners
 @core_app.get("/.well-known/lnurlp/{username}")
 async def lnaddress(username: str, request: Request):
     from lnbits.extensions.lnaddress.lnurl import lnurl_response
-
-    # domain = request.client
     domain = urlparse(str(request.url)).netloc
-    # print("client", root_url, request.client)
     return await lnurl_response(username, domain, request)
 
 
