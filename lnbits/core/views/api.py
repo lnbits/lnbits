435b4056fdd8c3c5f5fb00678fcbfc00fc411d20
Ben Arc <ben@arc.wales>
2022-03-10 12:16:51 +0000

    fixed conversion api

--- lnbits/core/views/api.py
+++ lnbits/core/views/api.py
@@ -573,6 +573,6 @@ async def api_fiat_as_sats(data: ConversionData):
         return output
     else:
         output[data.from_.upper()] = data.amount
-        output["sats"] = await fiat_amount_as_satoshis(data.amount, data._from)
+        output["sats"] = await fiat_amount_as_satoshis(data.amount, data.from_)
         output["BTC"] = output["sats"] / 100000000
         return output
