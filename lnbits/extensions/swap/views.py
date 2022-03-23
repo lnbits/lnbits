07c866a2cfc8cfb7dcf6015b4c37fc875ccae070
Tiago vasconcelos <talvasconcelos@gmail.com>
2022-03-23 12:35:58 +0000

    check for recurrent payments on open

--- lnbits/extensions/swap/views.py
+++ lnbits/extensions/swap/views.py
@@ -5,8 +5,10 @@ from fastapi.params import Depends
 from fastapi.templating import Jinja2Templates
 from starlette.responses import HTMLResponse
 
-from lnbits.core.models import User
+from lnbits.core.models import Payment, User
 from lnbits.decorators import check_user_exists
+from lnbits.extensions.swap.crud import get_recurrents
+from lnbits.extensions.swap.tasks import on_invoice_paid
 
 from . import swap_ext, swap_renderer
 
@@ -17,6 +19,23 @@ templates = Jinja2Templates(directory="templates")
 @swap_ext.get("/", response_class=HTMLResponse)
 async def index(request: Request, user: User = Depends(check_user_exists)):
     root_url = urlparse(str(request.url)).netloc
+    wallet_ids = [wallet.id for wallet in user.wallets]
+    recurrents = await get_recurrents(wallet_ids)
+    if recurrents:
+        for rec in recurrents:
+            mock_payment = Payment(
+                checking_id="mock",
+                pending=False,
+                amount=1,
+                fee=1,
+                time=0000,
+                bolt11="mock",
+                preimage="mock",
+                payment_hash="mock",
+                wallet_id=rec.wallet,
+            )
+            await on_invoice_paid(mock_payment)
+
     return swap_renderer().TemplateResponse("swap/index.html", {"request": request, "user": user.dict(), "root_url": root_url})
 
 
