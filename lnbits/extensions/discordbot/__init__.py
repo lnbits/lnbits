01c287f8842337e42ca00a62d00ef216b1efd213
ubuser <bitcoin_gamer_21@protonmail.com>
2022-03-25 11:05:33 +0100

    import staticfiles

--- lnbits/extensions/discordbot/__init__.py
+++ lnbits/extensions/discordbot/__init__.py
@@ -1,9 +1,18 @@
 from fastapi import APIRouter
+from fastapi.staticfiles import StaticFiles
 
 from lnbits.db import Database
 from lnbits.helpers import template_renderer
 
 db = Database("ext_discordbot")
+
+discordbot_static_files = [
+    {
+        "path": "/discordbot/static",
+        "app": StaticFiles(directory="lnbits/extensions/discordbot/static"),
+        "name": "discordbot_static",
+    }
+]
 
 discordbot_ext: APIRouter = APIRouter(prefix="/discordbot", tags=["discordbot"])
 
