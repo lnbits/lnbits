f08fbc2487948ef2fce951c5f55fa9bf714b76ff
callebtc <93376500+callebtc@users.noreply.github.com>
2022-05-10 21:48:24 +0200

    core: check_invoice_status for outgoing payments

--- lnbits/core/services.py
+++ lnbits/core/services.py
@@ -321,7 +321,10 @@ async def check_invoice_status(
     payment = await get_wallet_payment(wallet_id, payment_hash, conn=conn)
     if not payment:
         return PaymentStatus(None)
-    status = await WALLET.get_invoice_status(payment.checking_id)
+    if payment.is_out:
+        status = await WALLET.get_payment_status(payment.checking_id)
+    else:
+        status = await WALLET.get_invoice_status(payment.checking_id)
     if not payment.pending:
         return status
     if payment.is_out and status.failed:
