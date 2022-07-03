82d7bfbba8047cc5dbd25a9c86abc41501332fa5
Lee Salminen <leesalminen@gmail.com>
2022-07-02 21:16:27 -0600

    Enhancements to TPOS Extension
    - Add new tip % option to TPOS extension.
            - When adding a new TPOS, a user can choose 1 or more tip % options to be displayed to the customer.
            - When adding a new TPOS, a user can choose a wallet to send all collected tips to.
    
    - UI Refresh on TPOS extension.
            - Moved the share button to the top navigation, next to the TPOS name, and changed the icon to a more recognizable one.
            - Re-arranged the buttons on the keypad to be more ergonomic.

--- lnbits/extensions/tpos/__init__.py
+++ lnbits/extensions/tpos/__init__.py
@@ -1,7 +1,10 @@
+import asyncio
+
 from fastapi import APIRouter
 
 from lnbits.db import Database
 from lnbits.helpers import template_renderer
+from lnbits.tasks import catch_everything_and_restart
 
 db = Database("ext_tpos")
 
@@ -11,6 +14,10 @@ tpos_ext: APIRouter = APIRouter(prefix="/tpos", tags=["TPoS"])
 def tpos_renderer():
     return template_renderer(["lnbits/extensions/tpos/templates"])
 
-
+from .tasks import wait_for_paid_invoices
 from .views_api import *  # noqa
 from .views import *  # noqa
+
+def tpos_start():
+    loop = asyncio.get_event_loop()
+    loop.create_task(catch_everything_and_restart(wait_for_paid_invoices))
