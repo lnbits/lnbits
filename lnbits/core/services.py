7bfdeee4f49440fb431476bf81e2b73246981731
Tiago vasconcelos <talvasconcelos@gmail.com>
2022-07-05 16:09:31 +0100

    wallet type

--- lnbits/core/services.py
+++ lnbits/core/services.py
@@ -6,11 +6,13 @@ from typing import Dict, Optional, Tuple
 from urllib.parse import parse_qs, urlparse
 
 import httpx
+from fastapi import Depends
 from lnurl import LnurlErrorResponse
 from lnurl import decode as decode_lnurl  # type: ignore
 
 from lnbits import bolt11
 from lnbits.db import Connection
+from lnbits.decorators import WalletTypeInfo, require_admin_key
 from lnbits.helpers import url_for, urlsafe_short_hash
 from lnbits.requestvars import g
 from lnbits.settings import WALLET
@@ -243,7 +245,7 @@ async def redeem_lnurl_withdraw(
 
 
 async def perform_lnurlauth(
-    callback: str, wallet, conn: Optional[Connection] = None
+    callback: str, wallet: WalletTypeInfo = Depends(require_admin_key), conn: Optional[Connection] = None
 ) -> Optional[LnurlErrorResponse]:
     cb = urlparse(callback)
     
