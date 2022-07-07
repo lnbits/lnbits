ceca28f3491cd08e60a6697d44344f7abcaa14f6
callebtc <93376500+callebtc@users.noreply.github.com>
2022-07-07 14:01:05 +0200

    more logging

--- lnbits/__main__.py
+++ lnbits/__main__.py
@@ -13,8 +13,9 @@ from .settings import (
     LNBITS_SITE_TITLE,
     HOST,
     PORT,
-    SERVICE_FEE,
     WALLET,
+    LNBITS_DATABASE_URL,
+    LNBITS_DATA_FOLDER,
 )
 
 uvloop.install()
@@ -31,9 +32,11 @@ logger.info("Starting LNbits")
 logger.info(f"Host: {HOST}")
 logger.info(f"Port: {PORT}")
 logger.info(f"Debug: {DEBUG}")
-logger.info(f"Git version: {LNBITS_COMMIT}")
-logger.info(f"Data folder: {LNBITS_DATA_FOLDER}")
-logger.info(f"Funding source: {WALLET.__class__.__name__}")
-
 logger.info(f"Site title: {LNBITS_SITE_TITLE}")
+logger.info(f"Funding source: {WALLET.__class__.__name__}")
+logger.info(
+    f"Database: {'PostgreSQL' if LNBITS_DATABASE_URL.startswith('postgres://') else 'CockroachDB' if LNBITS_DATABASE_URL.startswith('cockroachdb://') else 'SQLite'}"
+)
+logger.info(f"Data folder: {LNBITS_DATA_FOLDER}")
+logger.info(f"Git version: {LNBITS_COMMIT}")
 # logger.info(f"Service fee: {SERVICE_FEE}")
