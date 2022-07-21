5d981f984ca67ba86bafcaca8d96ff116058d6dd
Black Coffee <randommw@protonmail.com>
2022-07-21 15:17:14 +0100

    Added extension list search

--- lnbits/core/static/js/extensions.js
+++ lnbits/core/static/js/extensions.js
@@ -1,4 +1,38 @@
 new Vue({
-  el: '#vue',
-  mixins: [windowMixin]
+    el: '#vue',
+    data: function () {
+        console.log(this)
+        return {
+            searchTerm: '',
+            filteredExtensions: null
+        }
+    },
+    mounted() {
+        this.filteredExtensions = this.g.extensions
+    },
+    watch: {
+        searchTerm(term) {
+            // Reset the filter
+            this.filteredExtensions = this.g.extensions
+            if(term !== "") { // Filter the extensions list
+                function extensionNameContains(searchTerm) {
+                    console.log('wordToCompare', searchTerm)
+                    return function (extension) {
+                        if(
+                            extension.name.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1
+                            ||
+                            extension.shortDescription.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1
+                        ) {
+                            return true
+                        }
+                        return false
+                    }
+                }
+
+                this.filteredExtensions = this.filteredExtensions
+                    .filter(extensionNameContains(term));
+            }
+        }
+    },
+    mixins: [windowMixin]
 })
