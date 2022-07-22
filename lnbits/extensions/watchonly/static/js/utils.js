cf8731f0d85154f45410abc28d23ea96bf64a679
Vlad Stan <stan.v.vlad@gmail.com>
2022-07-22 16:27:35 +0300

    feat: basic use of custom components

--- lnbits/extensions/watchonly/static/js/utils.js
+++ lnbits/extensions/watchonly/static/js/utils.js
@@ -139,3 +139,23 @@ const readFromSerialPort = serial => {
   }
   return readStringUntil
 }
+
+function loadTemplateAsync(path) {
+  const result = new Promise(resolve => {
+    const xhttp = new XMLHttpRequest()
+    console.log('### 300')
+
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4) {
+        if (this.status == 200) resolve(this.responseText)
+
+        if (this.status == 404) resolve('Page not found.')
+      }
+    }
+
+    xhttp.open('GET', path, true)
+    xhttp.send()
+  })
+
+  return result
+}
