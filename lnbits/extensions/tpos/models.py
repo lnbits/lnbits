ff5b1189b586f566e59004eb647a692cf46c2ff6
Tiago Vasconcelos <talvasconcelos@gmail.com>
2022-07-25 10:19:03 +0100

    Fix/tpos tips #697 (#713)
    * small fastapi convertion
    
    * make tip add up to invoice
    
    * make tip add to invoice
    
    * display existing tpos
    
    * clean console logs
    
    * suggestion from @leesalminen
    
    * blacked

--- lnbits/extensions/tpos/models.py
+++ lnbits/extensions/tpos/models.py
@@ -1,13 +1,15 @@
 from sqlite3 import Row
+from typing import Optional
 
+from fastapi import Query
 from pydantic import BaseModel
 
 
 class CreateTposData(BaseModel):
     name: str
     currency: str
-    tip_options: str
-    tip_wallet: str
+    tip_options: str = Query(None)
+    tip_wallet: str = Query(None)
 
 
 class TPoS(BaseModel):
@@ -15,8 +17,8 @@ class TPoS(BaseModel):
     wallet: str
     name: str
     currency: str
-    tip_options: str
-    tip_wallet: str
+    tip_options: Optional[str]
+    tip_wallet: Optional[str]
 
     @classmethod
     def from_row(cls, row: Row) -> "TPoS":
