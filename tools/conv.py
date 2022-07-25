6c28981bfd48ec5c8a4fcb883bfcff46f0441854
callebtc <93376500+callebtc@users.noreply.github.com>
2022-07-25 14:48:12 +0200

    cmon

--- tools/conv.py
+++ tools/conv.py
@@ -260,8 +260,9 @@ def migrate_ext(sqlite_db_file, schema, ignore_missing=True):
                 used,
                 usescsv,
                 webhook_url,
+                custom_url,
             )
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, to_timestamp(%s), %s, %s, %s);
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, to_timestamp(%s), %s, %s, %s, NULL);
         """
         insert_to_pg(q, res.fetchall())
         # WITHDRAW HASH CHECK
