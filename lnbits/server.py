41dc1b05125700f906920e352e77967c2c74791c
Tiago Vasconcelos <talvasconcelos@gmail.com>
2022-07-27 15:45:17 +0100

    Fix/hotfix tpos 2 (#783)
    * fix older tpos without tips
    
    * bad math requesting invoice
    
    * make format

--- lnbits/server.py
+++ lnbits/server.py
@@ -1,17 +1,18 @@
+import click
 import uvicorn
 
-import click
 
 @click.command()
-@click.option('--port', default="5000", help='Port to run LNBits on')
-@click.option('--host', default="127.0.0.1", help='Host to run LNBits on')
+@click.option("--port", default="5000", help="Port to run LNBits on")
+@click.option("--host", default="127.0.0.1", help="Host to run LNBits on")
 def main(port, host):
     """Launched with `poetry run lnbits` at root level"""
     uvicorn.run("lnbits.__main__:app", port=port, host=host)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
 
-#def main():
+# def main():
 #    """Launched with `poetry run start` at root level"""
 #    uvicorn.run("lnbits.__main__:app")
