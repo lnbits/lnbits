3054248858929e88a821fb597b3553c0029f7af5
Vlad Stan <stan.v.vlad@gmail.com>
2022-07-27 15:08:39 +0300

    feat: allow `Connect` from dropdown menu

--- lnbits/extensions/watchonly/static/components/wallet-config/wallet-config.js
+++ lnbits/extensions/watchonly/static/components/wallet-config/wallet-config.js
@@ -14,7 +14,6 @@ async function walletConfig(path) {
         return satOrBtc(val, showUnit, this.config.data.sats_denominated)
       },
       updateConfig: async function () {
-        // const wallet = this.g.user.wallets[0]
         try {
           await LNbits.api.request(
             'PUT',
