ce2ad774fb2934a783980bb4b0d6c0623203312d
Black Coffee <randommw@protonmail.com>
2022-07-28 16:00:48 +0100

    Removed keytransform method from StaticFilesHashCache

--- lnbits/helpers.py
+++ lnbits/helpers.py
@@ -50,22 +50,19 @@ class StaticFilesHashCache(MutableMapping):
         return self.get(file_path)
 
     def __getitem__(self, key):
-        return self.store[self._keytransform(key)]
+        return self.store[key]
 
     def __setitem__(self, key, value):
-        self.store[self._keytransform(key)] = value
+        self.store[key] = value
 
     def __delitem__(self, key):
-        del self.store[self._keytransform(key)]
+        del self.store[key]
 
     def __iter__(self):
         return iter(self.store)
 
     def __len__(self):
         return len(self.store)
-
-    def _keytransform(self, key):
-        return key
 
 class ExtensionManager:
     def __init__(self):
