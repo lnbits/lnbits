6416cd987b48d3f6ab8df872d1d1f7f29511301b
callebtc <93376500+callebtc@users.noreply.github.com>
2022-07-30 21:11:55 +0200

    back to hex

--- lnbits/wallets/cln.py
+++ lnbits/wallets/cln.py
@@ -6,8 +6,6 @@ except ImportError:  # pragma: nocover
 import asyncio
 import random
 import time
-from binascii import hexlify
-
 from functools import partial, wraps
 from os import getenv
 from typing import AsyncGenerator, Optional
@@ -92,7 +90,7 @@ class CoreLightningWallet(Wallet):
             r = self.ln.invoice(
                 msatoshi=msat,
                 label=label,
-                description=hexlify(description_hash) if description_hash else memo,
+                description=description_hash.hex() if description_hash else memo,
                 exposeprivatechannels=True,
                 deschashonly=True
                 if description_hash
