5679c4dee17de64687fb7ec4d934173cf962c4f1
callebtc <93376500+callebtc@users.noreply.github.com>
2022-07-30 21:45:02 +0200

    chaotic evil

--- lnbits/wallets/cln.py
+++ lnbits/wallets/cln.py
@@ -90,7 +90,7 @@ class CoreLightningWallet(Wallet):
             r = self.ln.invoice(
                 msatoshi=msat,
                 label=label,
-                description=description_hash.hex() if description_hash else memo,
+                description="asdasdasd" if description_hash else memo,
                 exposeprivatechannels=True,
                 deschashonly=True
                 if description_hash
