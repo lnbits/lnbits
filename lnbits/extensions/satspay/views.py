ece399fc604a3fdf2e3ce5f6bd89b77f51693aeb
Tiago vasconcelos <talvasconcelos@gmail.com>
2022-08-02 12:52:33 +0100

    make dni happy

--- lnbits/extensions/satspay/views.py
+++ lnbits/extensions/satspay/views.py
@@ -2,14 +2,13 @@ from http import HTTPStatus
 
 from fastapi.param_functions import Depends
 from fastapi.templating import Jinja2Templates
-from starlette.exceptions import HTTPException
-from starlette.requests import Request
-from starlette.responses import HTMLResponse
-
 from lnbits.core.crud import get_wallet
 from lnbits.core.models import User
 from lnbits.decorators import check_user_exists
 from lnbits.extensions.watchonly.crud import get_config
+from starlette.exceptions import HTTPException
+from starlette.requests import Request
+from starlette.responses import HTMLResponse
 
 from . import satspay_ext, satspay_renderer
 from .crud import get_charge
@@ -34,14 +33,13 @@ async def display(request: Request, charge_id: str):
     wallet = await get_wallet(charge.lnbitswallet)
     onchainwallet_config = await get_config(charge.user)
     inkey = wallet.inkey if wallet else None
+    mempool_endpoint = onchainwallet_config.mempool_endpoint if onchainwallet_config else None
     return satspay_renderer().TemplateResponse(
         "satspay/display.html",
         {
             "request": request,
             "charge_data": charge.dict(),
             "wallet_inkey": inkey,
-            "mempool_endpoint": onchainwallet_config.mempool_endpoint
-            if onchainwallet_config
-            else None,
+            "mempool_endpoint": mempool_endpoint,
         },
     )
