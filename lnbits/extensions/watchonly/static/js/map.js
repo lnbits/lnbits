cb137e7142c10868d7c61365df860cc926fa5a4c
Vlad Stan <stan.v.vlad@gmail.com>
2022-08-03 08:33:06 +0300

    fix: fingerprint is not unique per account (it is the fingerprint of the master)

--- lnbits/extensions/watchonly/static/js/map.js
+++ lnbits/extensions/watchonly/static/js/map.js
@@ -44,6 +44,7 @@ const mapUtxoToPsbtInput = utxo => ({
   branch_index: utxo.isChange ? 1 : 0,
   address_index: utxo.addressIndex,
   masterpub_fingerprint: utxo.masterpubFingerprint,
+  wallet: utxo.wallet,
   accountType: utxo.accountType,
   txHex: ''
 })
