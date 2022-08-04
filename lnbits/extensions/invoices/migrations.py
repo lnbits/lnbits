aab645b585d2cc07c06d8159116aac118d864c63
Tiago vasconcelos <talvasconcelos@gmail.com>
2022-08-04 16:34:13 +0100

    clean migrations

--- lnbits/extensions/invoices/migrations.py
+++ lnbits/extensions/invoices/migrations.py
@@ -1,6 +1,6 @@
 async def m001_initial_invoices(db):
 
-# STATUS COLUMN OPTIONS: 'draft', 'open', 'paid', 'canceled'
+    # STATUS COLUMN OPTIONS: 'draft', 'open', 'paid', 'canceled'
 
     await db.execute(
         f"""
@@ -53,61 +53,3 @@ async def m001_initial_invoices(db):
        );
    """
     )
-
-
-# async def m002_initial_invoices(db):
-#     await db.execute(
-#         f"""
-#        CREATE TABLE invoices.invoices (
-#            id TEXT PRIMARY KEY,
-#            wallet TEXT NOT NULL,
-
-#            status invoice_status_type DEFAULT 'draft',
-
-#            currency TEXT NOT NULL,
-
-#            company_name TEXT DEFAULT NULL,
-#            first_name TEXT DEFAULT NULL,
-#            last_name TEXT DEFAULT NULL,
-#            email TEXT DEFAULT NULL,
-#            phone TEXT DEFAULT NULL,
-#            address TEXT DEFAULT NULL,
-
-
-#            time TIMESTAMP NOT NULL DEFAULT {db.timestamp_now}
-#        );
-#    """
-#     )
-
-
-# async def m003_initial_invoice_items(db):
-#     await db.execute(
-#         f"""
-#        CREATE TABLE invoices.invoice_items (
-#            id TEXT PRIMARY KEY,
-#            invoice_id TEXT NOT NULL,
-
-#            description TEXT NOT NULL,
-#            amount INTEGER NOT NULL,
-
-#            FOREIGN KEY(invoice_id) REFERENCES {db.references_schema}invoices(id)
-#         );
-#    """
-#     )
-
-
-# async def m002_payments(db):
-#     await db.execute(
-#         f"""
-#        CREATE TABLE invoices.payments (
-#            id TEXT PRIMARY KEY,
-#            invoice_id TEXT NOT NULL,
-
-#            amount INT NOT NULL,
-
-#            time TIMESTAMP NOT NULL DEFAULT {db.timestamp_now},
-
-#            FOREIGN KEY(invoice_id) REFERENCES {db.references_schema}invoices(id)
-#        );
-#    """
-#     )
