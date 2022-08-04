a601042b3d3af60ae14e259944f2be1a68aa23c3
Tiago vasconcelos <talvasconcelos@gmail.com>
2022-08-04 11:08:11 +0100

    fixed failing bleskomat test, expecting 2000 msats fee

--- tests/extensions/bleskomat/test_lnurl_api.py
+++ tests/extensions/bleskomat/test_lnurl_api.py
@@ -133,4 +133,4 @@ async def test_bleskomat_lnurl_api_action_success(client, lnurl):
     assert wallet.balance_msat == 50000
     bleskomat_lnurl = await get_bleskomat_lnurl(secret)
     assert bleskomat_lnurl.has_uses_remaining() == False
-    WALLET.pay_invoice.assert_called_once_with(pr, 2000)
+    WALLET.pay_invoice.assert_called_once_with(pr, 4000)
