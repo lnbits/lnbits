c211273ba0650af08b4d4f70cfa9231e3e6ef0ec
Tiago vasconcelos <talvasconcelos@gmail.com>
2022-08-08 16:03:07 +0100

    new test and renaming

--- tests/extensions/invoices/test_invoices_api.py
+++ tests/extensions/invoices/test_invoices_api.py
@@ -2,14 +2,20 @@ import pytest
 import pytest_asyncio
 
 from lnbits.core.crud import get_wallet
-from tests.conftest import client, invoice
+from tests.conftest import client, inkey_headers_from, invoice
 from tests.extensions.invoices.conftest import invoice, invoices_wallet
 from tests.helpers import credit_wallet
 from tests.mocks import WALLET
 
 
 @pytest.mark.asyncio
-async def test_create_invoice_valid(client, invoices_wallet):
+async def test_invoices_unknown_invoice(client):
+    response = await client.get("invoices/pay/123")
+    assert response.status == 404
+    
+
+@pytest.mark.asyncio
+async def test_invoices_api_create_invoice_valid(client, invoices_wallet):
     query = {
         "status": "open",
         "currency": "EUR",
@@ -33,6 +39,7 @@ async def test_create_invoice_valid(client, invoices_wallet):
         json=query,
         headers={"X-Api-Key": invoices_wallet.inkey},
     )
+    
     assert response.status_code == 201
     data = response.json()
 
@@ -44,7 +51,7 @@ async def test_create_invoice_valid(client, invoices_wallet):
 
 
 @pytest.mark.asyncio
-async def test_partial_pay_invoice(client, invoices_wallet, invoice):
+async def test_invoices_api_partial_pay_invoice(client, invoices_wallet, invoice):
     invoice_id = invoice["id"]
     amount_to_pay = 5 * 100  # mock invoice is 10 USD
 
@@ -52,3 +59,4 @@ async def test_partial_pay_invoice(client, invoices_wallet, invoice):
         f"/invoices/api/v1/invoice/{invoice_id}/payments?famount={amount_to_pay}"
     )
     assert response.status_code == 201
+
