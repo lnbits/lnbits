0d88923976072bcefcacd9cb54da96966066d81b
dni <dni.khr@gmail.com>
2022-08-13 16:41:05 +0200

    fix subdomains extension mypy + 1 type error fixed

--- lnbits/extensions/subdomains/models.py
+++ lnbits/extensions/subdomains/models.py
@@ -1,26 +1,25 @@
 from fastapi.params import Query
 from pydantic.main import BaseModel
 
-
 class CreateDomain(BaseModel):
-    wallet: str = Query(...)
-    domain: str = Query(...)
-    cf_token: str = Query(...)
-    cf_zone_id: str = Query(...)
-    webhook: str = Query("")
-    description: str = Query(..., min_length=0)
-    cost: int = Query(..., ge=0)
-    allowed_record_types: str = Query(...)
+    wallet: str = Query(...) #type: ignore
+    domain: str = Query(...) #type: ignore
+    cf_token: str = Query(...) #type: ignore
+    cf_zone_id: str = Query(...) #type: ignore
+    webhook: str = Query("") #type: ignore
+    description: str = Query(..., min_length=0) #type: ignore
+    cost: int = Query(..., ge=0) #type: ignore
+    allowed_record_types: str = Query(...) #type: ignore
 
 
 class CreateSubdomain(BaseModel):
-    domain: str = Query(...)
-    subdomain: str = Query(...)
-    email: str = Query(...)
-    ip: str = Query(...)
-    sats: int = Query(..., ge=0)
-    duration: int = Query(...)
-    record_type: str = Query(...)
+    domain: str = Query(...) #type: ignore
+    subdomain: str = Query(...) #type: ignore
+    email: str = Query(...) #type: ignore
+    ip: str = Query(...) #type: ignore
+    sats: int = Query(..., ge=0) #type: ignore
+    duration: int = Query(...) #type: ignore
+    record_type: str = Query(...) #type: ignore
 
 
 class Domains(BaseModel):
