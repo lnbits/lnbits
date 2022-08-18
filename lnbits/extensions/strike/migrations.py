f46e90035f1cd87882a1184778aa591383080903
Lee Salminen <leesalminen@gmail.com>
2022-08-18 16:26:25 -0600

    not needed

--- lnbits/extensions/strike/migrations.py
+++ lnbits/extensions/strike/migrations.py
@@ -10,7 +10,7 @@ async def m001_initial(db):
             lnbits_wallet TEXT,
             handle TEXT,
             description TEXT,
-            api_key TEXT DEFAULT NULL,
+            api_key TEXT,
             currency TEXT,
             timestamp TIMESTAMP NOT NULL DEFAULT """
         + db.timestamp_now
