d8a13ed29d323f5233c6d9ba5cb59f9ef352d90c
ben <ben@arc.wales>
2022-09-21 15:31:31 +0100

    Added couple more tries

--- lnbits/decorators.py
+++ lnbits/decorators.py
@@ -239,13 +239,16 @@ async def check_user_exists(usr: UUID4) -> User:
     if LNBITS_ADMIN_UI:
         LNBITS_ADMIN_USERS = g().admin_conf.admin_users
         LNBITS_ALLOWED_USERS = g().admin_conf.allowed_users
-
-    if LNBITS_ALLOWED_USERS and g().user.id not in LNBITS_ALLOWED_USERS:
-        raise HTTPException(
-            status_code=HTTPStatus.UNAUTHORIZED, detail="User not authorized."
-        )
-
-    if LNBITS_ADMIN_USERS and g().user.id in LNBITS_ADMIN_USERS:
-        g().user.admin = True
-
+    try:
+        if LNBITS_ALLOWED_USERS and g().user.id not in LNBITS_ALLOWED_USERS:
+            raise HTTPException(
+                status_code=HTTPStatus.UNAUTHORIZED, detail="User not authorized."
+            )
+    except:
+        pass
+    try:
+        if LNBITS_ADMIN_USERS and g().user.id in LNBITS_ADMIN_USERS:
+            g().user.admin = True
+    except:
+        pass
     return g().user
