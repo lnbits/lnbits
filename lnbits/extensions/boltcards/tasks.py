b67012a3695ccc5a14266eea57b2e450a9dda496
callebtc <93376500+callebtc@users.noreply.github.com>
2022-09-27 15:35:38 +0200

    register named invoices

--- lnbits/extensions/boltcards/tasks.py
+++ lnbits/extensions/boltcards/tasks.py
@@ -6,13 +6,15 @@ import httpx
 from lnbits.core import db as core_db
 from lnbits.core.models import Payment
 from lnbits.tasks import register_invoice_listener
+from lnbits.helpers import get_current_extension_name
+
 
 from .crud import create_refund, get_hit
 
 
 async def wait_for_paid_invoices():
     invoice_queue = asyncio.Queue()
-    register_invoice_listener(invoice_queue)
+    register_invoice_listener(invoice_queue, get_current_extension_name())
 
     while True:
         payment = await invoice_queue.get()
--- lnbits/extensions/boltz/tasks.py
+++ lnbits/extensions/boltz/tasks.py
@@ -6,6 +6,8 @@ from loguru import logger
 from lnbits.core.models import Payment
 from lnbits.core.services import check_transaction_status
 from lnbits.tasks import register_invoice_listener
+from lnbits.helpers import get_current_extension_name
+
 
 from .boltz import (
     create_claim_tx,
@@ -127,7 +129,7 @@ async def check_for_pending_swaps():
 
 async def wait_for_paid_invoices():
     invoice_queue = asyncio.Queue()
-    register_invoice_listener(invoice_queue)
+    register_invoice_listener(invoice_queue, get_current_extension_name())
 
     while True:
         payment = await invoice_queue.get()
