c10f9f2576d4c7ee4b9e9d54bd7b3c671e6ab259
dni ⚡ <office@dnilabs.com>
2022-09-29 15:12:08 +0200

    remove weird comment

--- lnbits/core/static/js/service-worker.js
+++ lnbits/core/static/js/service-worker.js
@@ -30,7 +30,6 @@ self.addEventListener('activate', evt =>
 // If no response is found, it populates the runtime cache with the response
 // from the network before returning it to the page.
 self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
   if (
     !event.request.url.startsWith(
       self.location.origin + '/api/v1/payments/sse'
