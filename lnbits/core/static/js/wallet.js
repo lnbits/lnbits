0751ac4d37f2911c90f23e83b5443d5e72e1432a
ben <ben@arc.wales>
2022-09-29 21:46:49 +0100

    test

--- lnbits/core/static/js/wallet.js
+++ lnbits/core/static/js/wallet.js
@@ -220,21 +220,14 @@ new Vue({
     notificationToggle: function () {
       Notification.requestPermission().then((result) => {
         if (result === 'granted') {
-          randomNotification()
+          const options = {
+            body: "Notify body",
+          }
+          setTimeout(function() {
+            new Notification("Notify test", options)
+          }, 10000)
         }
       })
-    },
-    randomNotification: function () {
-      const randomItem = Math.floor(Math.random() * games.length)
-      const notifTitle = games[randomItem].name
-      const notifBody = `Created by ${games[randomItem].author}.`
-      const notifImg = `data/img/${games[randomItem].slug}.jpg`
-      const options = {
-        body: notifBody,
-        icon: notifImg,
-      }
-      new Notification(notifTitle, options)
-      setTimeout(randomNotification, 30000)
     },
     paymentTableRowKey: function (row) {
       return row.payment_hash + row.amount
