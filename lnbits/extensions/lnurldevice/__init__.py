0d1c42fba98e186a60037a46573cb351e929781a
callebtc <93376500+callebtc@users.noreply.github.com>
2022-10-10 16:05:31 +0200

    make format

--- lnbits/extensions/lnurldevice/__init__.py
+++ lnbits/extensions/lnurldevice/__init__.py
@@ -1,9 +1,10 @@
 import asyncio
+
 from fastapi import APIRouter
 
 from lnbits.db import Database
-from lnbits.tasks import catch_everything_and_restart
 from lnbits.helpers import template_renderer
+from lnbits.tasks import catch_everything_and_restart
 
 db = Database("ext_lnurldevice")
 
@@ -14,8 +15,8 @@ def lnurldevice_renderer():
     return template_renderer(["lnbits/extensions/lnurldevice/templates"])
 
 
-from .tasks import wait_for_paid_invoices
 from .lnurl import *  # noqa
+from .tasks import wait_for_paid_invoices
 from .views import *  # noqa
 from .views_api import *  # noqa
 
