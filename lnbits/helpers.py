79a0c28f23fd225a71a3186dbd6ca59076e38401
callebtc <93376500+callebtc@users.noreply.github.com>
2022-10-12 23:17:21 +0200

    allow external migration modules

--- lnbits/helpers.py
+++ lnbits/helpers.py
@@ -20,6 +20,8 @@ class Extension(NamedTuple):
     icon: Optional[str] = None
     contributors: Optional[List[str]] = None
     hidden: bool = False
+    migration_module: Optional[str] = None
+    db_name: Optional[str] = None
 
 
 class ExtensionManager:
@@ -66,6 +68,8 @@ class ExtensionManager:
                     config.get("icon"),
                     config.get("contributors"),
                     config.get("hidden") or False,
+                    config.get("migration_module"),
+                    config.get("db_name"),
                 )
             )
 
