9a904201a782ca610d768386966092e485886908
Vlad Stan <stan.v.vlad@gmail.com>
2022-10-17 15:05:43 +0300

    feat: add `LedgerCrud`

--- lnbits/extensions/cashu/crud.py
+++ lnbits/extensions/cashu/crud.py
@@ -12,7 +12,7 @@ from lnbits.helpers import urlsafe_short_hash
 
 from . import db
 
-from .models import Cashu, Pegs, Promises, Proof
+from .models import Cashu, Invoice, Pegs, Promises, Proof
 
 from cashu.core.base import MintKeyset
 from lnbits.db import Database, Connection
@@ -88,25 +88,57 @@ async def delete_cashu(cashu_id) -> None:
 # ###############MINT STUFF#################
 # ##########################################
 
+class LedgerCrud:
+    """
+    Database interface for Cashu mint.
 
-# async def store_promises(
-#     amounts: List[int], B_s: List[str], C_s: List[str], cashu_id: str
-# ):
-#     for amount, B_, C_ in zip(amounts, B_s, C_s):
-#         await store_promise(amount, B_, C_, cashu_id)
+    This class needs to be overloaded by any app that imports the Cashu mint.
+    """
+
+    async def get_keyset(*args, **kwags):
+        return await get_keyset(*args, **kwags)
+
+    async def get_lightning_invoice(*args, **kwags):
+        return await get_lightning_invoice(*args, **kwags)
+
+    async def get_proofs_used(*args, **kwags):
+        return await get_proofs_used(*args, **kwags)
+
+    async def invalidate_proof(*args, **kwags):
+        return await invalidate_proof(*args, **kwags)
+
+    async def store_keyset(*args, **kwags):
+        return await store_keyset(*args, **kwags)
+
+    async def store_lightning_invoice(*args, **kwags):
+        return await store_lightning_invoice(*args, **kwags)
+
+    async def store_promise(*args, **kwags):
+        return await store_promise(*args, **kwags)
+
+    async def update_lightning_invoice(*args, **kwags):
+        return await update_lightning_invoice(*args, **kwags)
 
 
-# async def store_promise(amount: int, B_: str, C_: str, cashu_id: str):
-#     promise_id = urlsafe_short_hash()
 
-#     await db.execute(
-#         """
-#         INSERT INTO cashu.promises
-#           (id, amount, B_b, C_b, cashu_id)
-#         VALUES (?, ?, ?, ?, ?)
-#         """,
-#         (promise_id, amount, str(B_), str(C_), cashu_id),
-#     )
+async def store_promises(
+    amounts: List[int], B_s: List[str], C_s: List[str], cashu_id: str
+):
+    for amount, B_, C_ in zip(amounts, B_s, C_s):
+        await store_promise(amount, B_, C_, cashu_id)
+
+
+async def store_promise(amount: int, B_: str, C_: str, cashu_id: str):
+    promise_id = urlsafe_short_hash()
+
+    await db.execute(
+        """
+        INSERT INTO cashu.promises
+          (id, amount, B_b, C_b, cashu_id)
+        VALUES (?, ?, ?, ?, ?)
+        """,
+        (promise_id, amount, str(B_), str(C_), cashu_id),
+    )
 
 
 # async def get_promises(cashu_id) -> Optional[Cashu]:
@@ -116,29 +148,29 @@ async def delete_cashu(cashu_id) -> None:
 #     return Promises(**row) if row else None
 
 
-# async def get_proofs_used(
-#     db: Database,
-#     conn: Optional[Connection] = None,
-# ):
+async def get_proofs_used(
+    db: Database,
+    conn: Optional[Connection] = None,
+):
 
-#     rows = await (conn or db).fetchall(
-#         """
-#         SELECT secret from cashu.proofs_used
-#         """
-#     )
-#     return [row[0] for row in rows]
+    rows = await (conn or db).fetchall(
+        """
+        SELECT secret from cashu.proofs_used
+        """
+    )
+    return [row[0] for row in rows]
 
 
-# async def invalidate_proof(cashu_id: str, proof: Proof):
-#     invalidate_proof_id = urlsafe_short_hash()
-#     await db.execute(
-#         """
-#         INSERT INTO cashu.proofs_used
-#           (id, amount, C, secret, cashu_id)
-#         VALUES (?, ?, ?, ?, ?)
-#         """,
-#         (invalidate_proof_id, proof.amount, str(proof.C), str(proof.secret), cashu_id),
-#     )
+async def invalidate_proof(cashu_id: str, proof: Proof):
+    invalidate_proof_id = urlsafe_short_hash()
+    await db.execute(
+        """
+        INSERT INTO cashu.proofs_used
+          (id, amount, C, secret, cashu_id)
+        VALUES (?, ?, ?, ?, ?)
+        """,
+        (invalidate_proof_id, proof.amount, str(proof.C), str(proof.secret), cashu_id),
+    )
 
 
 # ########################################
@@ -146,46 +178,46 @@ async def delete_cashu(cashu_id) -> None:
 # ########################################
 
 
-# async def store_lightning_invoice(cashu_id: str, invoice: Invoice):
-#     await db.execute(
-#         """
-#         INSERT INTO cashu.invoices
-#           (cashu_id, amount, pr, hash, issued)
-#         VALUES (?, ?, ?, ?, ?)
-#         """,
-#         (
-#             cashu_id,
-#             invoice.amount,
-#             invoice.pr,
-#             invoice.hash,
-#             invoice.issued,
-#         ),
-#     )
+async def store_lightning_invoice(cashu_id: str, invoice: Invoice):
+    await db.execute(
+        """
+        INSERT INTO cashu.invoices
+          (cashu_id, amount, pr, hash, issued)
+        VALUES (?, ?, ?, ?, ?)
+        """,
+        (
+            cashu_id,
+            invoice.amount,
+            invoice.pr,
+            invoice.hash,
+            invoice.issued,
+        ),
+    )
 
 
-# async def get_lightning_invoice(cashu_id: str, hash: str):
-#     row = await db.fetchone(
-#         """
-#         SELECT * from cashu.invoices
-#         WHERE cashu_id =? AND hash = ?
-#         """,
-#         (
-#             cashu_id,
-#             hash,
-#         ),
-#     )
-#     return Invoice.from_row(row)
+async def get_lightning_invoice(cashu_id: str, hash: str):
+    row = await db.fetchone(
+        """
+        SELECT * from cashu.invoices
+        WHERE cashu_id =? AND hash = ?
+        """,
+        (
+            cashu_id,
+            hash,
+        ),
+    )
+    return Invoice.from_row(row)
 
 
-# async def update_lightning_invoice(cashu_id: str, hash: str, issued: bool):
-#     await db.execute(
-#         "UPDATE cashu.invoices SET issued = ? WHERE cashu_id = ? AND hash = ?",
-#         (
-#             issued,
-#             cashu_id,
-#             hash,
-#         ),
-#     )
+async def update_lightning_invoice(cashu_id: str, hash: str, issued: bool):
+    await db.execute(
+        "UPDATE cashu.invoices SET issued = ? WHERE cashu_id = ? AND hash = ?",
+        (
+            issued,
+            cashu_id,
+            hash,
+        ),
+    )
 
 
 ##############################
@@ -193,55 +225,55 @@ async def delete_cashu(cashu_id) -> None:
 ##############################
 
 
-# async def store_keyset(
-#     keyset: MintKeyset,
-#     db: Database = None,
-#     conn: Optional[Connection] = None,
-# ):
+async def store_keyset(
+    keyset: MintKeyset,
+    db: Database = None,
+    conn: Optional[Connection] = None,
+):
 
-#     await (conn or db).execute(  # type: ignore
-#         """
-#         INSERT INTO cashu.keysets
-#           (id, derivation_path, valid_from, valid_to, first_seen, active, version)
-#         VALUES (?, ?, ?, ?, ?, ?, ?)
-#         """,
-#         (
-#             keyset.id,
-#             keyset.derivation_path,
-#             keyset.valid_from or db.timestamp_now,
-#             keyset.valid_to or db.timestamp_now,
-#             keyset.first_seen or db.timestamp_now,
-#             True,
-#             keyset.version,
-#         ),
-#     )
+    await (conn or db).execute(  # type: ignore
+        """
+        INSERT INTO cashu.keysets
+          (id, derivation_path, valid_from, valid_to, first_seen, active, version)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+        """,
+        (
+            keyset.id,
+            keyset.derivation_path,
+            keyset.valid_from or db.timestamp_now,
+            keyset.valid_to or db.timestamp_now,
+            keyset.first_seen or db.timestamp_now,
+            True,
+            keyset.version,
+        ),
+    )
 
 
-# async def get_keyset(
-#     id: str = None,
-#     derivation_path: str = "",
-#     db: Database = None,
-#     conn: Optional[Connection] = None,
-# ):
-#     clauses = []
-#     values: List[Any] = []
-#     clauses.append("active = ?")
-#     values.append(True)
-#     if id:
-#         clauses.append("id = ?")
-#         values.append(id)
-#     if derivation_path:
-#         clauses.append("derivation_path = ?")
-#         values.append(derivation_path)
-#     where = ""
-#     if clauses:
-#         where = f"WHERE {' AND '.join(clauses)}"
+async def get_keyset(
+    id: str = None,
+    derivation_path: str = "",
+    db: Database = None,
+    conn: Optional[Connection] = None,
+):
+    clauses = []
+    values: List[Any] = []
+    clauses.append("active = ?")
+    values.append(True)
+    if id:
+        clauses.append("id = ?")
+        values.append(id)
+    if derivation_path:
+        clauses.append("derivation_path = ?")
+        values.append(derivation_path)
+    where = ""
+    if clauses:
+        where = f"WHERE {' AND '.join(clauses)}"
 
-#     rows = await (conn or db).fetchall(  # type: ignore
-#         f"""
-#         SELECT * from cashu.keysets
-#         {where}
-#         """,
-#         tuple(values),
-#     )
-#     return [MintKeyset.from_row(row) for row in rows]
+    rows = await (conn or db).fetchall(  # type: ignore
+        f"""
+        SELECT * from cashu.keysets
+        {where}
+        """,
+        tuple(values),
+    )
+    return [MintKeyset.from_row(row) for row in rows]
