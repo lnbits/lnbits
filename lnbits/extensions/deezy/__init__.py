43a974af838944ae2e77b576ac6b848dfabdd5fa
Uthpala Heenatigala <uiheenatigala@gmail.com>
2022-11-29 23:18:24 +0100

    initializing the deezy extension

---
+++ lnbits/extensions/deezy/__init__.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter
+
+from lnbits.db import Database
+from lnbits.helpers import template_renderer
+
+db = Database("ext_deezy")
+
+deezy_ext: APIRouter = APIRouter(prefix="/deezy", tags=["deezy"])
+
+
+def deezy_renderer():
+    return template_renderer(["lnbits/extensions/deezy/templates"])
+
+
+from .views import *  # noqa
+from .views_api import *  # noqa
---
+++ lnbits/extensions/deezy/models.py
@@ -0,0 +1,5 @@
+# from pydantic import BaseModel
+
+# class Example(BaseModel):
+#    id: str
+#    wallet: str
