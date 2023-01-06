cc375c0e408e54a7e35cfcab394a8218eb89dd30
callebtc <93376500+callebtc@users.noreply.github.com>
2023-01-06 13:41:41 +0100

    clean up

--- lnbits/extensions/splitpayments/tasks.py
+++ lnbits/extensions/splitpayments/tasks.py
@@ -26,51 +26,54 @@ async def on_invoice_paid(payment: Payment) -> None:
         return
 
     targets = await get_targets(payment.wallet_id)
-    logger.debug(targets)
+
     if not targets:
         return
 
+    # validate target percentages
     total_percent = sum([target.percent for target in targets])
 
     if total_percent > 100:
-        logger.error("splitpayment failure: total percent adds up to more than 100%")
+        logger.error("splitpayment: total percent adds up to more than 100%")
+        return
+    if not all([target.percent > 0 for target in targets]):
+        logger.error("splitpayment: not all percentages are positive > 0")
         return
 
-    logger.debug(f"performing split payments to {len(targets)} targets")
-
-    amount_to_split = payment.amount
+    logger.trace(f"splitpayments: performing split payments to {len(targets)} targets")
 
     if payment.extra.get("amount"):
         amount_to_split = (payment.extra.get("amount") or 0) * 1000
+    else:
+        amount_to_split = payment.amount
+
+    if not amount_to_split:
+        logger.error("splitpayments: no amount to split")
+        return
 
     for target in targets:
-        if target.percent > 0:
-            tagged = target.tag in payment.extra
+        tagged = target.tag in payment.extra
 
-            amount = int(amount_to_split * target.percent / 100)
+        if tagged:
+            memo = f"Pushed tagged payment to {target.alias}"
+            amount_msat = int(amount_to_split)
+        else:
+            amount_msat = int(amount_to_split * target.percent / 100)
             memo = (
-                f"split payment: {target.percent}% for {target.alias or target.wallet}"
-            )
-            if tagged:
-                memo = f"Pushed tagged payment to {target.alias}"
-                amount = int(amount_to_split)
-
-            payment_hash, payment_request = await create_invoice(
-                wallet_id=target.wallet,
-                amount=int(amount / 1000),
-                internal=True,
-                memo=memo,
+                f"Split payment: {target.percent}% for {target.alias or target.wallet}"
             )
 
-            logger.debug(f"created split invoice: {payment_hash}")
+        payment_hash, payment_request = await create_invoice(
+            wallet_id=target.wallet,
+            amount=int(amount_msat / 1000),
+            internal=True,
+            memo=memo,
+        )
 
-            extra = {**payment.extra, "splitted": True}
+        extra = {**payment.extra, "splitted": True}
 
-            checking_id = await pay_invoice(
-                payment_request=payment_request,
-                wallet_id=payment.wallet_id,
-                extra=extra,
-            )
-            logger.debug(f"paid split invoice: {checking_id}")
-
-    logger.debug(f"performing split to {len(targets)} targets")
+        await pay_invoice(
+            payment_request=payment_request,
+            wallet_id=payment.wallet_id,
+            extra=extra,
+        )
