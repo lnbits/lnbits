bbb955df1b73ca65a25d1d81be6164576520a5ff
ben <ben@arc.wales>
2023-01-06 10:57:15 +0000

    Its alive!

--- lnbits/extensions/example/__init__.py
+++ lnbits/extensions/example/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from fastapi.staticfiles import StaticFiles
 
 from lnbits.db import Database
 from lnbits.helpers import template_renderer
@@ -6,6 +7,14 @@ from lnbits.helpers import template_renderer
 db = Database("ext_example")
 
 example_ext: APIRouter = APIRouter(prefix="/example", tags=["example"])
+
+example_static_files = [
+    {
+        "path": "/example/static",
+        "app": StaticFiles(packages=[("lnbits", "extensions/example/static")]),
+        "name": "example_static",
+    }
+]
 
 
 def example_renderer():
