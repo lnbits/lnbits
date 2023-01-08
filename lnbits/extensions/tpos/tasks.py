aa067df380e4311b05ae2c82421bbaf71340f20e
Pavol Rusnak <pavol@rusnak.io>
2023-01-08 12:19:58 +0000

    remove extraneous not payment.extra check
    followup to fca7e7bec3a40de8a5f7f65ac6030513c02e8db7

--- lnbits/extensions/tpos/tasks.py
+++ lnbits/extensions/tpos/tasks.py
@@ -20,9 +20,6 @@ async def wait_for_paid_invoices():
 
 
 async def on_invoice_paid(payment: Payment) -> None:
-    if not payment.extra:
-        return
-
     if payment.extra.get("tag") != "tpos":
         return
 
