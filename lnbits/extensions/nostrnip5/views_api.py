96ed9192a3854fc38cd01463d9ebe61aa0939405
ben <ben@arc.wales>
2023-01-09 11:01:15 +0000

    format

--- lnbits/extensions/nostrnip5/views_api.py
+++ lnbits/extensions/nostrnip5/views_api.py
@@ -199,7 +199,9 @@ async def api_address_create(
     if domain.currency == "Satoshis":
         price_in_sats = domain.amount
     else:
-        price_in_sats = await fiat_amount_as_satoshis(domain.amount / 100, domain.currency)
+        price_in_sats = await fiat_amount_as_satoshis(
+            domain.amount / 100, domain.currency
+        )
 
     try:
         payment_hash, payment_request = await create_invoice(
