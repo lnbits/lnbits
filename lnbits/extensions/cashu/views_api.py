c33e54d280cf445a54e01f897950e59751b96401
callebtc <93376500+callebtc@users.noreply.github.com>
2023-01-25 13:23:55 +0100

    do not reference these object

--- lnbits/extensions/cashu/views_api.py
+++ lnbits/extensions/cashu/views_api.py
@@ -343,13 +343,13 @@ async def melt_coins(
             else:
                 logger.debug(f"Cashu: Payment failed for {invoice_obj.payment_hash}")
     except Exception as e:
-        logger.debug(f"Cashu: Exception for {invoice_obj.payment_hash}: {str(e)}")
+        logger.debug(f"Cashu: Exception: {str(e)}")
         raise HTTPException(
             status_code=HTTPStatus.BAD_REQUEST,
             detail=f"Cashu: {str(e)}",
         )
     finally:
-        logger.debug(f"Cashu: Unset pending for {invoice_obj.payment_hash}")
+        logger.debug(f"Cashu: Unset pending")
         # delete proofs from pending list
         await ledger._unset_proofs_pending(proofs)
 
