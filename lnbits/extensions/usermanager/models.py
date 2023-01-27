013e533d2e874feb0a18555b91003975a9b4e68a
Sam Korn <korn94sam@gmail.com>
2023-01-27 12:53:42 -0700

    update field descriptions

--- lnbits/extensions/usermanager/models.py
+++ lnbits/extensions/usermanager/models.py
@@ -2,21 +2,21 @@ from sqlite3 import Row
 from typing import Optional
 
 from fastapi.param_functions import Query
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class CreateUserData(BaseModel):
-    user_name: str = Query(...)
-    wallet_name: str = Query(...)
-    admin_id: str = Query(...)
+    user_name: str = Query(..., description="Name of the user")
+    wallet_name: str = Query(..., description="Name of the user")
+    admin_id: str = Query(..., description="Id of the user which will administer this new user")
     email: str = Query("")
     password: str = Query("")
 
 
 class CreateUserWallet(BaseModel):
-    user_id: str = Query(...)
-    wallet_name: str = Query(...)
-    admin_id: str = Query(...)
+    user_id: str = Query(..., description="Target user for this new wallet")
+    wallet_name: str = Query(..., description="Name of the new wallet to create")
+    admin_id: str = Query(..., description="Id of the user which will administer this new wallet")
 
 
 class User(BaseModel):
