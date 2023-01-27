5c16d750a028ab3f61abf1e449cb59b63b489082
Sam Korn <korn94sam@gmail.com>
2023-01-27 13:41:22 -0700

    format and fix tests

--- lnbits/extensions/usermanager/views_api.py
+++ lnbits/extensions/usermanager/views_api.py
@@ -8,7 +8,7 @@ from starlette.exceptions import HTTPException
 
 from lnbits.core import update_user_extension
 from lnbits.core.crud import get_user
-from lnbits.core.models import Payment
+from lnbits.core.models import Payment, User
 from lnbits.decorators import WalletTypeInfo, get_key_type, require_admin_key
 
 from . import usermanager_ext
@@ -24,7 +24,7 @@ from .crud import (
     get_usermanager_wallet_transactions,
     get_usermanager_wallets,
 )
-from .models import CreateUserData, CreateUserWallet, User, Wallet
+from .models import CreateUserData, CreateUserWallet, Wallet
 
 
 @usermanager_ext.get(
