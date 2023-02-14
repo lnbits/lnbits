3276347d5de9aa499f8267b89cdf8322bb89567c
dni ⚡ <office@dnilabs.com>
2023-02-14 12:42:46 +0100

    remove all extensions from migration data at once (#1489)
    * remove all extnesions from migration data at once
    
    * nerf workflow for now
    
    * check dbversions

--- tools/conv.py
+++ tools/conv.py
@@ -212,7 +212,8 @@ print("Selected path: ", args.sqlite_path)
 if os.path.isdir(args.sqlite_path):
     exclude_tables = ["dbversions"]
     file = os.path.join(args.sqlite_path, "database.sqlite3")
-    check_db_versions(file)
+    # TODO
+    # check_db_versions(file)
     if not args.extensions_only:
         migrate_core(file, exclude_tables)
 
@@ -223,8 +224,11 @@ if os.path.isdir(args.sqlite_path):
 else:
     files = [args.sqlite_path]
 
-excluded_exts = ["ext_lnurlpos.sqlite3"]
-for file in files:
-    filename = os.path.basename(file)
-    if filename.startswith("ext_") and filename not in excluded_exts:
-        migrate_ext(file)
+
+# TODO: think about testing extension migrations again,
+#       after we pulled them out from core
+# excluded_exts = ["ext_lnurlpos.sqlite3"]
+# for file in files:
+#     filename = os.path.basename(file)
+#     if filename.startswith("ext_") and filename not in excluded_exts:
+#         migrate_ext(file)
