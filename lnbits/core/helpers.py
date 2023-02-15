7ef4c18b56340df0bdf849137678637651cee32e
Vlad Stan <stan.v.vlad@gmail.com>
2023-02-15 17:31:10 +0200

    doc: update coments

--- lnbits/core/helpers.py
+++ lnbits/core/helpers.py
@@ -48,8 +48,9 @@ async def run_migration(db: Connection, migrations_module: Any, current_version:
 
 async def stop_extension_background_work(ext_id: str, user: str):
     """
-    Stop background workk for extension (like asyncio.Tasks, WebSockets, etc)
-    It tries first to call the endpoint using `http` and if ti fails it tries using `https`
+    Stop background work for extension (like asyncio.Tasks, WebSockets, etc)
+    Extensions SHOULD expoze a DELETE enpoint at the root level of their API.
+    This function tries first to call the endpoint using `http` and if if fails it tries using `https`.
     """
     async with httpx.AsyncClient() as client:
         try:
