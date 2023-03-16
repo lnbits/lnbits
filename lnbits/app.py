601592a7c34cbedb88b971a93c4a71df858aa48e
dni ⚡ <office@dnilabs.com>
2023-03-16 17:29:18 +0100

    move version into settings

--- lnbits/app.py
+++ lnbits/app.py
@@ -50,8 +50,6 @@ from .tasks import (
     webhook_handler,
 )
 
-__version__ = importlib.metadata.version("lnbits")
-
 
 def create_app() -> FastAPI:
 
@@ -60,7 +58,7 @@ def create_app() -> FastAPI:
     app = FastAPI(
         title="LNbits API",
         description="API for LNbits, the free and open source bitcoin wallet and accounts system with plugins.",
-        version=__version__,
+        version=settings.version,
         license_info={
             "name": "MIT License",
             "url": "https://raw.githubusercontent.com/lnbits/lnbits/main/LICENSE",
@@ -309,7 +307,7 @@ def register_startup(app: FastAPI):
 
 def log_server_info():
     logger.info("Starting LNbits")
-    logger.info(f"Version: {__version__}")
+    logger.info(f"Version: {settings.version}")
     logger.info(f"Baseurl: {settings.lnbits_baseurl}")
     logger.info(f"Host: {settings.host}")
     logger.info(f"Port: {settings.port}")
