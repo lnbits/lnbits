0b596c00ca51ee373555bba0c0fbf5987c706359
dni ⚡ <office@dnilabs.com>
2023-04-17 08:29:01 +0200

    reduce max string length

--- lnbits/wallets/void.py
+++ lnbits/wallets/void.py
@@ -23,9 +23,10 @@ class VoidWallet(Wallet):
         raise Unsupported("")
 
     async def status(self) -> StatusResponse:
-        logger.warning(
-            "This backend does nothing, it is here just as a placeholder, you must configure an actual backend before being able to do anything useful with LNbits."
-        )
+        logger.warning((
+            "This backend does nothing, it is here just as a placeholder, you must configure an "
+            "actual backend before being able to do anything useful with LNbits."
+        ))
         return StatusResponse(None, 0)
 
     async def pay_invoice(self, bolt11: str, fee_limit_msat: int) -> PaymentResponse:
