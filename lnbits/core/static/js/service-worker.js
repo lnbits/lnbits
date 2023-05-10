117f9f0346ca6ff562bc971f3c37718c52630e0e
ben <ben@arc.wales>
2023-05-10 13:20:43 +0100

    Added rounded style

--- lnbits/core/static/js/service-worker.js
+++ lnbits/core/static/js/service-worker.js
@@ -1,6 +1,6 @@
 // update cache version every time there is a new deployment
 // so the service worker reinitializes the cache
-const CACHE_VERSION = 10
+const CACHE_VERSION = 11
 const CURRENT_CACHE = `lnbits-${CACHE_VERSION}-`
 
 const getApiKey = request => {
