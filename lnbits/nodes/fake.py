c9154594c2808a6b91990e677be8a54793623404
jackstar12 <jkranawetter05@gmail.com>
2023-05-11 12:27:27 +0200

    fake wallet

---
+++ lnbits/nodes/fake.py
@@ -0,0 +1,90 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from lnbits.nodes.base import ChannelStats, Node, PaymentStats
+
+from .base import NodeChannel, NodeChannelsResponse, NodeInfoResponse, NodePayment
+
+if TYPE_CHECKING:
+    from lnbits.wallets import FakeWallet
+
+
+class FakeNode(Node):
+    wallet: FakeWallet
+
+    async def _get_id(self) -> str:
+        return "026165850492521f4ac8abd9bd8088123446d126f648ca35e60f88177dc149ceb2"
+
+    async def get_channels(self) -> NodeChannelsResponse:
+        return NodeChannelsResponse(
+            error_message=None,
+            channels=[
+                NodeChannel(
+                    peer_id="fake",
+                    inbound_msat=10000,
+                    outbound_msat=10000,
+                    total_msat=20000,
+                    name="cool fake node",
+                    color="#821212",
+                )
+            ],
+        )
+
+    async def get_info(self) -> NodeInfoResponse:
+        info = await self.wallet.ln_rpc("getinfo")
+        channel_response = await self.get_channels()
+        channels = channel_response.channels
+        return NodeInfoResponse(
+            id=info["id"],
+            backend_name="Fake",
+            alias=info["alias"],
+            color=info["color"],
+            onchain_balance_sat=100000000,
+            onchain_confirmed_sat=100000000,
+            # A future implementation could leverage the `sql` rpc to calculate these
+            # without having to fetch all the channels.
+            channel_stats=ChannelStats.from_list(channels),
+            num_peers=info["num_peers"],
+            blockheight=info["blockheight"],
+            balance_msat=sum(channel.inbound_msat for channel in channels),
+            channels=channels,
+        )
+
+    async def get_payments(self) -> list[NodePayment]:
+        pays = await self.wallet.ln_rpc("listpays")
+        invoices = await self.wallet.ln_rpc("listinvoices")
+        results = []
+
+        results.extend(
+            NodePayment(
+                bolt11=pay["bolt11"],
+                amount=int(pay["amount_msat"]) * -1,
+                fee=int(pay["amount_msat"]) - int(pay["amount_sent_msat"]),
+                memo=pay.get("description"),
+                time=pay["created_at"],
+                preimage=pay["preimage"],
+                payment_hash=pay["payment_hash"],
+                pending=pay["status"] != "complete",
+            )
+            for pay in pays["pays"]
+            if pay["status"] != "failed"
+        )
+        results.extend(
+            NodePayment(
+                bolt11=invoice["bolt11"],
+                amount=invoice["amount_msat"],
+                # fee=pay["amount_sent_msat"] - pay["amount_msat"],
+                preimage=invoice.get("payment_preimage") or "0" * 64,
+                memo=invoice["description"],
+                time=invoice.get("paid_at", invoice["expires_at"]),
+                payment_hash=invoice["payment_hash"],
+                pending=invoice["status"] != "paid",
+            )
+            for invoice in invoices["invoices"]
+        )
+        results.sort(key=lambda x: x.time, reverse=True)
+        return results
+
+    async def get_payment_stats(self) -> PaymentStats:
+        return PaymentStats()
