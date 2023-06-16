ec55bf44dadcfa83fb9f733cf91285258b3e543d
jackstar12 <jkranawetter05@gmail.com>
2023-06-16 19:18:49 +0200

    test nested query

--- tests/core/test_db.py
+++ tests/core/test_db.py
@@ -3,11 +3,18 @@ import pytest
 
 @pytest.mark.asyncio
 async def test_sql_json(db, json_test_table):
-    obj = {"a": "foo", "b": "bar"}
+    obj = {"a": "foo", "b": "bar", "c": {"nested": "d"}}
     await db.execute("INSERT INTO test VALUES(?)", (obj,))
 
     row = await db.fetchone(
-        f"SELECT * FROM test WHERE {db.json_path('data', 'b')} = ?", (obj["b"],)
+        f"SELECT * FROM test WHERE {db.json_path('data', 'a')} = ?",
+        (obj["a"],),
+    )
+    assert row.data == obj
+
+    row = await db.fetchone(
+        f"SELECT * FROM test WHERE {db.json_path('data', 'c', 'nested')} = ?",
+        (obj["c"]["nested"],),
     )
     assert row.data == obj
 
