b9db3eafe0a237b89655cd5f4b597df96df32c97
callebtc <93376500+callebtc@users.noreply.github.com>
2023-07-03 11:12:09 +0200

    remove sleep (#1797)

--- lnbits/middleware.py
+++ lnbits/middleware.py
@@ -1,4 +1,3 @@
-import asyncio
 from http import HTTPStatus
 from typing import Any, List, Tuple, Union
 from urllib.parse import parse_qs
@@ -215,7 +214,6 @@ def add_ip_block_middleware(app: FastAPI):
             request.client.host in settings.lnbits_blocked_ips
             and request.client.host not in settings.lnbits_allowed_ips
         ):
-            await asyncio.sleep(5)
             return JSONResponse(
                 status_code=403,  # Forbidden
                 content={"detail": "IP is blocked"},
