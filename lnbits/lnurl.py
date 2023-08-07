9f51502893be4ef827033ebe3a516d01fa23b533
dni ⚡ <office@dnilabs.com>
2023-08-07 18:26:57 +0200

    [FEAT] updating to latest lnurl lib and use it more
    WIP

--- lnbits/lnurl.py
+++ lnbits/lnurl.py
@@ -1,17 +1,17 @@
-from bech32 import bech32_decode, bech32_encode, convertbits
+from lnurl import decode, encode, handle  # noqa: F401
+
+# from bech32 import bech32_decode, bech32_encode, convertbits
+# def decode(lnurl: str) -> str:
+#     hrp, data = bech32_decode(lnurl)
+#     assert hrp
+#     assert data
+#     bech32_data = convertbits(data, 5, 8, False)
+#     assert bech32_data
+#     return bytes(bech32_data).decode()
 
 
-def decode(lnurl: str) -> str:
-    hrp, data = bech32_decode(lnurl)
-    assert hrp
-    assert data
-    bech32_data = convertbits(data, 5, 8, False)
-    assert bech32_data
-    return bytes(bech32_data).decode()
-
-
-def encode(url: str) -> str:
-    bech32_data = convertbits(url.encode(), 8, 5, True)
-    assert bech32_data
-    lnurl = bech32_encode("lnurl", bech32_data)
-    return lnurl.upper()
+# def encode(url: str) -> str:
+#     bech32_data = convertbits(url.encode(), 8, 5, True)
+#     assert bech32_data
+#     lnurl = bech32_encode("lnurl", bech32_data)
+#     return lnurl.upper()
