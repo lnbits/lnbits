cb7287c9860667b5669df505f7fee1ea55ceff7f
dni ⚡ <office@dnilabs.com>
2023-08-10 00:49:44 +0200

    revert breaking changes wait for PR #1876
    https://github.com/lnbits/lnbits/pull/1876

--- lnbits/core/__init__.py
+++ lnbits/core/__init__.py
@@ -3,13 +3,13 @@ from fastapi.routing import APIRouter
 from lnbits.core.models import CoreAppExtra
 from lnbits.db import Database
 
-from .views.admin_api import *  # noqa: F401,F403
-from .views.api import *  # noqa: F401,F403
-from .views.generic import *  # noqa: F401,F403
-from .views.public_api import *  # noqa: F401,F403
-
 db = Database("database")
 
 core_app: APIRouter = APIRouter()
 
 core_app_extra: CoreAppExtra = CoreAppExtra()
+
+from .views.admin_api import *  # noqa: F401,F403
+from .views.api import *  # noqa: F401,F403
+from .views.generic import *  # noqa: F401,F403
+from .views.public_api import *  # noqa: F401,F403
