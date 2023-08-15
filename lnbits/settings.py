fe174284019da325d41aed890079f3b81e3e466c
jacksn <jkranawetter05@gmail.com>
2023-08-15 13:55:06 +0200

    comments for node settings

--- lnbits/settings.py
+++ lnbits/settings.py
@@ -221,8 +221,11 @@ class FundingSourcesSettings(
 
 
 class NodeUISettings(LNbitsSettings):
+    # on-off switch for node ui
     lnbits_node_ui: bool = Field(default=False)
+    # whether to display the public node ui (only if lnbits_node_ui is True)
     lnbits_public_node_ui: bool = Field(default=False)
+    # can be used to disable the transactions tab in the node ui (recommended for large cln nodes)
     lnbits_node_ui_transactions: bool = Field(default=True)
 
 
