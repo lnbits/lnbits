4e2b1c7ef783c6522631f305f190426d9db62364
jacksn <jkranawetter05@gmail.com>
2023-08-15 15:08:16 +0200

    turn transactions tab off by default

--- lnbits/settings.py
+++ lnbits/settings.py
@@ -226,7 +226,7 @@ class NodeUISettings(LNbitsSettings):
     # whether to display the public node ui (only if lnbits_node_ui is True)
     lnbits_public_node_ui: bool = Field(default=False)
     # can be used to disable the transactions tab in the node ui (recommended for large cln nodes)
-    lnbits_node_ui_transactions: bool = Field(default=True)
+    lnbits_node_ui_transactions: bool = Field(default=False)
 
 
 class EditableSettings(
