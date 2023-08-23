3ce2549b164dcfa8c7d7b86e3fd7a9a4ec3df81c
callebtc <93376500+callebtc@users.noreply.github.com>
2023-08-23 16:25:57 +0200

    add Breez SDK wallet

--- lnbits/settings.py
+++ lnbits/settings.py
@@ -230,6 +230,12 @@ class LnTipsFundingSource(LNbitsSettings):
     lntips_invoice_key: Optional[str] = Field(default=None)
 
 
+class BreezSdkFundingSource(LNbitsSettings):
+    breez_api_key: Optional[str] = Field(default=None)
+    breez_greenlight_seed: Optional[str] = Field(default=None)
+    breez_greenlight_invite_code: Optional[str] = Field(default=None)
+
+
 class LightningSettings(LNbitsSettings):
     lightning_invoice_expiry: int = Field(default=3600)
 
@@ -248,6 +254,7 @@ class FundingSourcesSettings(
     OpenNodeFundingSource,
     SparkFundingSource,
     LnTipsFundingSource,
+    BreezSdkFundingSource,
 ):
     lnbits_backend_wallet_class: str = Field(default="VoidWallet")
 
@@ -401,6 +408,7 @@ class SuperUserSettings(LNbitsSettings):
             "AlbyWallet",
             "LNbitsWallet",
             "OpenNodeWallet",
+            "BreezSdkWallet",
         ]
     )
 
