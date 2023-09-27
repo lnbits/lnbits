69703b0a0519293919c756507ba1206d84964e84
dni ⚡ <office@dnilabs.com>
2023-09-27 10:47:55 +0200

    [BUG] bundle is broken (#1972)

--- lnbits/core/static/js/service-worker.js
+++ lnbits/core/static/js/service-worker.js
@@ -1,6 +1,6 @@
 // update cache version every time there is a new deployment
 // so the service worker reinitializes the cache
-const CACHE_VERSION = 55
+const CACHE_VERSION = 56
 const CURRENT_CACHE = `lnbits-${CACHE_VERSION}-`
 
 const getApiKey = request => {
