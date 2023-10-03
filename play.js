d7126a86b86cef097599e640de76324f574bd683
Vlad Stan <stan.v.vlad@gmail.com>
2023-10-03 15:23:01 +0300

    chore: extract service names

--- play.js
+++ play.js
@@ -6,37 +6,50 @@ const data = fs.readFileSync('sample.config.nix', 'utf8')
 const lines = data.split('\n')
 console.log('### data', lines.length)
 
-const optionsPrefix = '  options ='
-const serviceNamePrefix = '    services.'
+const indentSpaceCount = 2
 
 const result = {
   service: ''
 }
 
-for (let i = 0; i < lines.length; i++) {
-  if (lines[i].startsWith(optionsPrefix)) {
-    handleService(extractObject(lines.slice(i + 1), 4))
+handleData(lines, indentSpaceCount)
+
+function handleData(lines, depth) {
+  const nextDepth = depth + indentSpaceCount
+  for (let i = 0; i < lines.length; i++) {
+    const servicesPrefix = nested('options.services.', depth)
+    if (lines[i].startsWith(servicesPrefix)) {
+      result.service = lines[i].substring(servicesPrefix.length).split(' ')[0]
+      handleOptions(extractObject(lines.slice(i + 1), nextDepth), nextDepth)
+      return
+    }
+    if (lines[i].startsWith(nested('options', depth))) {
+      handleService(extractObject(lines.slice(i + 1), nextDepth), nextDepth)
+      return
+    }
   }
 }
 
-function handleService(lines) {
+function handleService(lines, depth) {
+  const nextDepth = depth + indentSpaceCount
+  const serviceNamePrefix = nested('services.', depth)
   for (let i = 0; i < lines.length; i++) {
     if (lines[i].startsWith(serviceNamePrefix)) {
       result.service = lines[i]
         .substring(serviceNamePrefix.length)
         .split(' ')[0]
     }
-    handleOptions(extractObject(lines.slice(i + 1), 4))
+    handleOptions(extractObject(lines.slice(i + 1), nextDepth), nextDepth)
     return
   }
 }
 
-function handleOptions(lines) {
-    console.log('### lines', lines.join('\n'))
+function handleOptions(lines, depth) {
+  console.log('### option.lines', lines.length, depth)
 }
 
 function extractObject(lines, nestingLevel) {
-  const prefix = Array(nestingLevel).fill(' ').join('')
+  const prefix = nested('', nestingLevel)
   const objectLines = []
   for (const line of lines) {
     if (line.startsWith(prefix)) {
@@ -46,6 +59,11 @@ function extractObject(lines, nestingLevel) {
     }
   }
   return objectLines
+}
+
+function nested(str, level) {
+  const prefix = Array(level).fill(' ').join('')
+  return prefix + str
 }
 
 console.log('### result', result)
