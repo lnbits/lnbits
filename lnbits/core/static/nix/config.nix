19d773ced26fae8837131d31f930b586fb4954d6
Arc <ben@arc.wales>
2023-10-05 12:24:36 +0100

    Made warning more explicit

--- lnbits/core/static/nix/config.nix
+++ lnbits/core/static/nix/config.nix
@@ -1,4 +1,7 @@
-########DANGER: YOU CAN BREAK YOU SYSTEM WITH THIS FORM########
+
+### DANGER: YOU CAN BREAK YOU SYSTEM WITH THIS FORM ###
+
+
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
