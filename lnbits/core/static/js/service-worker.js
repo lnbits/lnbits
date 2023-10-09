2c396d36c2deddb0158e8fcca76aa689a9fcdea3
Pavol Rusnak <pavol@rusnak.io>
2023-10-09 16:20:10 +0200

    remove white border around logo in the QR

--- lnbits/core/static/js/service-worker.js
+++ lnbits/core/static/js/service-worker.js
@@ -1,6 +1,6 @@
 // update cache version every time there is a new deployment
 // so the service worker reinitializes the cache
-const CACHE_VERSION = 60
+const CACHE_VERSION = 61
 const CURRENT_CACHE = `lnbits-${CACHE_VERSION}-`
 
 const getApiKey = request => {
