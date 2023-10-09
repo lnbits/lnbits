e036eb7aa034a795ccd7687fae13b7663217d7c3
dni ⚡ <office@dnilabs.com>
2023-10-09 10:52:18 +0200

    use <lnbits-dynamic-fields>

--- lnbits/static/js/components/extension-settings.js
+++ lnbits/static/js/components/extension-settings.js
@@ -1,61 +1,43 @@
 Vue.component('extension-settings', {
   name: 'extension-settings',
-  props: ['settings-data', 'settings-endpoint'],
+  props: ['options', 'name'],
   data: function () {
     return {
-      interalSettings: this.settingsData,
+      settings: undefined,
       usr: undefined,
       admin: false,
       show: false
     }
   },
-  computed: {
-    settings: {
-      get() {
-        return this.internalSettings
-      },
-      set(value) {
-        value.isLoaded = true
-        this.internalSettings = JSON.parse(JSON.stringify(value))
-        this.$emit(
-          'update:settings-data',
-          JSON.parse(JSON.stringify(this.internalSettings))
-        )
-      }
-    }
-  },
   methods: {
+    endpoint: function () {
+      return `/${this.name}/api/v1/settings?usr=${this.usr}`
+    },
     updateSettings: async function () {
       try {
         const {data} = await LNbits.api.request(
           'PUT',
-          this.settingsEndpoint + '?usr=' + window.user.id,
+          this.endpoint(),
           null,
           this.interalSettings
         )
         this.show = false
-        this.interalSettings = data
+        this.settings = data
       } catch (error) {
         LNbits.utils.notifyApiError(error)
       }
     },
     getSettings: async function () {
       try {
-        const {data} = await LNbits.api.request(
-          'GET',
-          this.settingsEndpoint + '?usr=' + this.usr
-        )
-        this.interalSettings = data
+        const {data} = await LNbits.api.request('GET', this.endpoint())
+        this.settings = data
       } catch (error) {
         LNbits.utils.notifyApiError(error)
       }
     },
     resetSettings: async function () {
       try {
-        await LNbits.api.request(
-          'DELETE',
-          this.settingsEndpoint + '?usr=' + this.usr
-        )
+        await LNbits.api.request('DELETE', this.endpoint())
         this.getSettings()
       } catch (error) {
         LNbits.utils.notifyApiError(error)
@@ -70,11 +52,11 @@ Vue.component('extension-settings', {
     }
   },
   template: `
-    <q-btn v-if="admin" unelevated @click="show = true" color="primary" icon="settings" class="float-right">
+    <q-btn v-if="admin && settings" unelevated @click="show = true" color="primary" icon="settings" class="float-right">
         <q-dialog v-model="show" position="top">
           <q-card class="q-pa-lg q-pt-xl lnbits__dialog-card">
             <q-form @submit="updateSettings" class="q-gutter-md">
-              <slot v-bind:settings="settings"></slot>
+              <lnbits-dynamic-fields :options="options" v-model="settings"></lnbits-dynamic-fields>
               <div class="row q-mt-lg">
                 <q-btn unelevated color="primary" type="submit">Update</q-btn>
                 <q-btn unelevated color="danger" @click="resetSettings" >Reset</q-btn>
