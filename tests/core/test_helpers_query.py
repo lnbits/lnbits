d364f868d03e2ef78b2cf2217204ef0dd6717940
dni ⚡ <office@dnilabs.com>
2023-10-09 13:39:39 +0200

    add tests
    add tests

---
+++ tests/core/test_helpers_query.py
@@ -0,0 +1,27 @@
+import pytest
+from pydantic import BaseModel
+
+from lnbits.helpers import (
+    insert_query,
+    update_query,
+)
+
+
+class TestModel(BaseModel):
+    id: int
+    name: str
+
+
+test = TestModel(id=1, name="test")
+
+
+@pytest.mark.asyncio
+async def test_helpers_insert_query():
+    q = insert_query("test_helpers_query", test)
+    assert q == "INSERT INTO test_helpers_query (id, name) VALUES (?, ?)"
+
+
+@pytest.mark.asyncio
+async def test_helpers_update_query():
+    q = update_query("test_helpers_query", test)
+    assert q == "UPDATE test_helpers_query SET (id = ?, name = ?) WHERE id = ?"
