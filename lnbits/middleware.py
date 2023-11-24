3fa937acc9d92ec2a5922f0f792adf79ef36c3f0
Vlad Stan <stan.v.vlad@gmail.com>
2023-11-24 18:17:16 +0200

    fix: corner case for root path

--- lnbits/middleware.py
+++ lnbits/middleware.py
@@ -25,7 +25,8 @@ class InstalledExtensionMiddleware:
         self.app = app
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
-        if "path" not in scope:
+        path = scope.get("path", "/")
+        if path == "/":
             await self.app(scope, receive, send)
             return
 
