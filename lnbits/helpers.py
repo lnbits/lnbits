f9359ac449dc1843109e7b02deda939e24afbd76
Vlad Stan <stan.v.vlad@gmail.com>
2023-12-12 09:11:29 +0200

    fix: more relaxed email verification (by @prusnak)
    Co-authored-by: Pavol Rusnak <pavol@rusnak.io>

--- lnbits/helpers.py
+++ lnbits/helpers.py
@@ -173,7 +173,7 @@ def update_query(table_name: str, model: BaseModel, where: str = "WHERE id = ?")
 
 
 def is_valid_email_address(email: str) -> bool:
-    email_regex = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b"
+    email_regex = r"[A-Za-z0-9\._%+-]+@[A-Za-z0-9\.-]+\.[A-Za-z]{2,63}"
     return re.fullmatch(email_regex, email) is not None
 
 
