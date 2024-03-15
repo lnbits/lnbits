c162da41cdd2c75c3f551059eb7f13aa4f2d7c65
Vlad Stan <stan.v.vlad@gmail.com>
2024-03-15 17:42:04 +0200

    test: more `CoreLightningRestWallet` tests

--- tests/wallets/test_corelightningrest.py
+++ tests/wallets/test_corelightningrest.py
@@ -1,5 +1,9 @@
+from json.decoder import JSONDecodeError
+
 import pytest
+from httpx import HTTPStatusError
 from pytest_httpserver import HTTPServer
+from werkzeug.wrappers import Response
 
 from lnbits.wallets.corelightningrest import CoreLightningRestWallet, settings
 
@@ -44,3 +48,104 @@ async def test_status_with_balance(httpserver: HTTPServer):
     assert status.balance_msat == 55000
 
     httpserver.check_assertions()
+
+
+@pytest.mark.asyncio
+async def test_status_with_error(httpserver: HTTPServer):
+    settings.corelightning_rest_url = ENDPONT
+    settings.corelightning_rest_macaroon = MCAROON
+
+    resp = {"error": "test-error"}
+    httpserver.expect_request("/v1/channel/localremotebal").respond_with_json(resp)
+
+    wallet = CoreLightningRestWallet()
+
+    status = await wallet.status()
+    assert (
+        status.error_message == f"Failed to connect to {ENDPONT}, got: 'test-error...'"
+    )
+    assert status.balance_msat == 0
+
+    httpserver.check_assertions()
+
+
+@pytest.mark.asyncio
+async def test_status_with_error_body(httpserver: HTTPServer):
+    settings.corelightning_rest_url = ENDPONT
+    settings.corelightning_rest_macaroon = MCAROON
+
+    httpserver.expect_request("/v1/channel/localremotebal").respond_with_data(
+        "test-text-error"
+    )
+
+    wallet = CoreLightningRestWallet()
+
+    with pytest.raises(JSONDecodeError) as e_info:
+        await wallet.status()
+
+    assert str(e_info.value) == "Expecting value: line 1 column 1 (char 0)"
+
+    httpserver.check_assertions()
+
+
+@pytest.mark.asyncio
+async def test_status_for_http_404(httpserver: HTTPServer):
+    settings.corelightning_rest_url = ENDPONT
+    settings.corelightning_rest_macaroon = MCAROON
+
+    httpserver.expect_request("/v1/channel/localremotebal").respond_with_response(
+        Response("Not Found", status=404)
+    )
+
+    wallet = CoreLightningRestWallet()
+
+    with pytest.raises(HTTPStatusError) as e_info:
+        await wallet.status()
+
+    assert e_info.match("Client error '404 NOT FOUND'")
+
+    httpserver.check_assertions()
+
+
+@pytest.mark.asyncio
+async def test_status_for_server_down():
+    settings.corelightning_rest_url = ENDPONT
+    settings.corelightning_rest_macaroon = MCAROON
+
+    wallet = CoreLightningRestWallet()
+
+    with pytest.raises(HTTPStatusError) as e_info:
+        await wallet.status()
+
+    assert e_info.match("Server error '500 INTERNAL SERVER ERROR'")
+
+
+@pytest.mark.asyncio
+async def test_status_for_missing_config():
+    settings.corelightning_rest_url = None
+    settings.corelightning_rest_macaroon = None
+
+    with pytest.raises(ValueError) as e_info:
+        CoreLightningRestWallet()
+
+    assert (
+        str(e_info.value)
+        == "cannot initialize CoreLightningRestWallet: "
+        + "missing corelightning_rest_url"
+    )
+
+    settings.corelightning_rest_url = ENDPONT
+
+    with pytest.raises(ValueError) as e_info:
+        CoreLightningRestWallet()
+
+    assert (
+        str(e_info.value)
+        == "cannot initialize CoreLightningRestWallet: "
+        + "missing corelightning_rest_macaroon"
+    )
+
+    settings.corelightning_rest_macaroon = MCAROON
+
+    wallet = CoreLightningRestWallet()
+    assert wallet.url == ENDPONT
