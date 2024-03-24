fa2a8216a2b2f8c423c47ef5fdcbc4d8f0472d0f
benarc <ben@arc.wales>
2024-03-24 11:44:49 +0000

    0.12.3 hotfix revert

--- lnbits/wallets/base.py
+++ lnbits/wallets/base.py
@@ -35,7 +35,7 @@ class PaymentStatus(NamedTuple):
 
     @property
     def pending(self) -> bool:
-        return self.paid is None
+        return self.paid is not True
 
     @property
     def failed(self) -> bool:
