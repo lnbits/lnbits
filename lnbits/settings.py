31ec24f106200486eb5644fc42bf7ca5a3e1938b
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2024-05-04 11:43:51 +0200

    feat: Blink funding source

--- lnbits/settings.py
+++ lnbits/settings.py
@@ -214,6 +214,11 @@ class LnPayFundingSource(LNbitsSettings):
     lnpay_admin_key: Optional[str] = Field(default=None)
 
 
+class BlinkFundingSource(LNbitsSettings):
+    blink_api_endpoint: Optional[str] = Field(default=None)
+    blink_token: Optional[str] = Field(default=None)
+
+
 class ZBDFundingSource(LNbitsSettings):
     zbd_api_endpoint: Optional[str] = Field(default="https://api.zebedee.io/v0/")
     zbd_api_key: Optional[str] = Field(default=None)
@@ -262,6 +267,7 @@ class FundingSourcesSettings(
     LndRestFundingSource,
     LndGrpcFundingSource,
     LnPayFundingSource,
+    BlinkFundingSource,
     AlbyFundingSource,
     ZBDFundingSource,
     PhoenixdFundingSource,
@@ -411,14 +417,15 @@ class SuperUserSettings(LNbitsSettings):
     lnbits_allowed_funding_sources: list[str] = Field(
         default=[
             "AlbyWallet",
-            "FakeWallet",
+            "BlinkWallet",
             "CoreLightningRestWallet",
             "CoreLightningWallet",
             "EclairWallet",
-            "LNbitsWallet",
-            "LndRestWallet",
+            "FakeWallet",
             "LNPayWallet",
+            "LNbitsWallet",
             "LnTipsWallet",
+            "LndRestWallet",
             "LndWallet",
             "OpenNodeWallet",
             "PhoenixdWallet",
