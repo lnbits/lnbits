5d91d035e8fe51d742c9d8e209045102de640ab8
dni ⚡ <office@dnilabs.com>
2024-05-10 19:40:08 +0200

    add more tests

--- tests/regtest/test_services_create_invoice.py
+++ tests/regtest/test_services_create_invoice.py
@@ -1,9 +1,12 @@
 import pytest
 from bolt11 import decode
+
 from lnbits.core.services import (
     create_invoice,
 )
+
 descrtiption = "test create invoice"
+
 
 @pytest.mark.asyncio
 async def test_create_invoice(from_wallet):
@@ -21,10 +24,7 @@ async def test_create_invoice(from_wallet):
 @pytest.mark.asyncio
 async def test_create_internal_invoice(from_wallet):
     payment_hash, pr = await create_invoice(
-        wallet_id=from_wallet.id,
-        amount=1000,
-        memo=descrtiption,
-        internal=True
+        wallet_id=from_wallet.id, amount=1000, memo=descrtiption, internal=True
     )
     invoice = decode(pr)
     assert invoice.payment_hash == payment_hash
--- tests/regtest/test_services_pay_invoice.py
+++ tests/regtest/test_services_pay_invoice.py
@@ -1,10 +1,45 @@
 import pytest
+
+from lnbits.core.crud import (
+    get_standalone_payment,
+)
 from lnbits.core.services import (
+    PaymentError,
     pay_invoice,
 )
 
 description = "test pay invoice"
 
+
 @pytest.mark.asyncio
 async def test_services_pay_invoice(to_wallet, real_invoice):
-    res = await pay_invoice(wallet_id=to_wallet.id, payment_request=real_invoice, description=description)
+    payment_hash = await pay_invoice(
+        wallet_id=to_wallet.id,
+        payment_request=real_invoice.get("bolt11"),
+        description=description,
+    )
+    assert payment_hash
+    payment = await get_standalone_payment(payment_hash)
+    assert payment
+    assert not payment.pending
+    assert payment.memo == description
+
+
+@pytest.mark.asyncio
+async def test_services_pay_invoice_invalid_bolt11(to_wallet):
+    with pytest.raises(PaymentError):
+        await pay_invoice(
+            wallet_id=to_wallet.id,
+            payment_request="lnbcr1123123n",
+        )
+
+
+@pytest.mark.asyncio
+async def test_services_pay_invoice_0_amount_invoice(
+    to_wallet, real_amountless_invoice
+):
+    with pytest.raises(PaymentError):
+        await pay_invoice(
+            wallet_id=to_wallet.id,
+            payment_request=real_amountless_invoice,
+        )
