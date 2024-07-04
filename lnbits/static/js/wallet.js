b194dd33c0d54315a10e3a8ed6bc440c2e7dac4d
schneimi <233221+schneimi@users.noreply.github.com>
2024-07-04 23:34:41 +0200

    Update wallet.js
    enable 'Read' button after 'Paste from clipboard' click

--- lnbits/static/js/wallet.js
+++ lnbits/static/js/wallet.js
@@ -542,7 +542,7 @@ new Vue({
     pasteToTextArea: function () {
       this.$refs.textArea.focus() // Set cursor to textarea
       navigator.clipboard.readText().then(text => {
-        this.$refs.textArea.value = text
+        this.parse.data.request = text.trim()
       })
     }
   },
