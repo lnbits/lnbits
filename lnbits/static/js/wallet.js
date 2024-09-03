lnbits/lnbits

commit 28df100d9a86d15cfae0e45971c95557c018c90e

Fix lnurl fallback (#2656)
* Fix lnurl fallback

Wrong string case search

Closes #1599

* optimize the code

* one more clean up

lnbits/static/js/wallet.js
@@ -219,20 +219,16 @@ new Vue({
     decodeRequest: function () {
       this.parse.show = true
       let req = this.parse.data.request.toLowerCase()
-      if (this.parse.data.request.toLowerCase().startsWith('lightning:')) {
-        this.parse.data.request = this.parse.data.request.slice(10)
-      } else if (this.parse.data.request.toLowerCase().startsWith('lnurl:')) {
-        this.parse.data.request = this.parse.data.request.slice(6)
-      } else if (req.indexOf('lightning=lnurl1') !== -1) {
-        this.parse.data.request = this.parse.data.request
-          .split('lightning=')[1]
-          .split('&')[0]
+
+      if (req.startsWith('lightning:')) {
+        this.parse.data.request = req.slice(10)
+      } else if (req.startsWith('lnurl:')) {
+        this.parse.data.request = req.slice(6)
+      } else if (req.includes('lightning=lnurl1')) {
+        this.parse.data.request = req.split('lightning=')[1].split('&')[0]
       }
 
-      if (
-        this.parse.data.request.toLowerCase().startsWith('lnurl1') ||
-        this.parse.data.request.match(/[\w.+-~_]+@[\w.+-~_]/)
-      ) {
+      if (req.startsWith('lnurl1') || req.match(/[\w.+-~_]+@[\w.+-~_]/)) {
         LNbits.api
           .request(
             'GET',

commit d704ba47492b04405ff8397743d6bba6fd3470f0

fix fuck up on refactor
Bug introduced in #2656

lnbits/static/js/wallet.js
@@ -219,7 +219,6 @@ new Vue({
     decodeRequest: function () {
       this.parse.show = true
       let req = this.parse.data.request.toLowerCase()
-
       if (req.startsWith('lightning:')) {
         this.parse.data.request = req.slice(10)
       } else if (req.startsWith('lnurl:')) {
@@ -227,7 +226,7 @@ new Vue({
       } else if (req.includes('lightning=lnurl1')) {
         this.parse.data.request = req.split('lightning=')[1].split('&')[0]
       }
-
+      req = this.parse.data.request
       if (req.startsWith('lnurl1') || req.match(/[\w.+-~_]+@[\w.+-~_]/)) {
         LNbits.api
           .request(