ee397bb636a1a7eea848b838ae57fdd1c8a9de53
Vlad Stan <stan.v.vlad@gmail.com>
2024-09-04 16:58:18 +0300

    test: add more tests

--- tests/unit/test_settings.py
+++ tests/unit/test_settings.py
@@ -19,37 +19,72 @@ nostrrelay_redirect_path = {
 }
 
 
+@pytest.fixture()
+def lnurlp():
+    return RedirectPath(ext_id="lnurlp", **lnurlp_redirect_path)
+
+
+@pytest.fixture()
+def lnaddress():
+    return RedirectPath(ext_id="lnaddress", **lnaddress_redirect_path)
+
+
+@pytest.fixture()
+def nostrrelay():
+    return RedirectPath(ext_id="nostrrelay", **nostrrelay_redirect_path)
+
+
 @pytest.mark.asyncio
-async def test_redirect_path_in_conflict():
-
-    redirect_path_lnurl = RedirectPath(ext_id="lnurlp", **lnurlp_redirect_path)
-    redirect_path_lnaddress = RedirectPath(
-        ext_id="lnaddress", **lnaddress_redirect_path
-    )
-    nostrrelay_path_lnurl = RedirectPath(ext_id="lnurlp", **nostrrelay_redirect_path)
-
-    assert not redirect_path_lnurl.in_conflict(
-        redirect_path_lnurl
-    ), "Path is not in conflict with itself."
-    assert not redirect_path_lnaddress.in_conflict(
-        redirect_path_lnaddress
-    ), "Path is not in conflict with itself."
-    assert not nostrrelay_path_lnurl.in_conflict(
-        nostrrelay_path_lnurl
+async def test_redirect_path_self_not_in_conflict(
+    lnurlp: RedirectPath, lnaddress: RedirectPath, nostrrelay: RedirectPath
+):
+    assert not lnurlp.in_conflict(lnurlp), "Path is not in conflict with itself."
+    assert not lnaddress.in_conflict(lnaddress), "Path is not in conflict with itself."
+    assert not nostrrelay.in_conflict(
+        nostrrelay
     ), "Path is not in conflict with itself."
 
-    assert redirect_path_lnurl.in_conflict(
-        redirect_path_lnaddress
-    ), "Paths are in conflict."
+    assert not lnurlp.in_conflict(nostrrelay), "Paths are not in conflict."
 
-    assert redirect_path_lnaddress.in_conflict(
-        redirect_path_lnurl
-    ), "Paths are in conflict (commutative)."
+    assert not nostrrelay.in_conflict(lnurlp), "Paths are not in conflict."
 
-    assert not redirect_path_lnurl.in_conflict(
-        nostrrelay_path_lnurl
+
+@pytest.mark.asyncio
+async def test_redirect_path_not_in_conflict(
+    lnurlp: RedirectPath, lnaddress: RedirectPath, nostrrelay: RedirectPath
+):
+
+    assert not lnurlp.in_conflict(nostrrelay), "Paths are not in conflict."
+
+    assert not nostrrelay.in_conflict(lnurlp), "Paths are not in conflict."
+
+    assert not lnaddress.in_conflict(nostrrelay), "Paths are not in conflict."
+
+    assert not nostrrelay.in_conflict(lnaddress), "Paths are not in conflict."
+
+
+@pytest.mark.asyncio
+async def test_redirect_path_in_conflict(lnurlp: RedirectPath, lnaddress: RedirectPath):
+    assert lnurlp.in_conflict(lnaddress), "Paths are in conflict."
+    assert lnaddress.in_conflict(lnurlp), "Paths are in conflict."
+
+
+@pytest.mark.asyncio
+async def test_redirect_path_find_conflict(
+    lnurlp: RedirectPath, lnaddress: RedirectPath, nostrrelay: RedirectPath
+):
+    assert lnurlp.find_in_conflict([nostrrelay, lnaddress]), "Paths are in conflict."
+    assert lnurlp.find_in_conflict([lnaddress, nostrrelay]), "Paths are in conflict."
+    assert lnaddress.find_in_conflict([nostrrelay, lnurlp]), "Paths are in conflict."
+    assert lnaddress.find_in_conflict([lnurlp, nostrrelay]), "Paths are in conflict."
+
+
+@pytest.mark.asyncio
+async def test_redirect_path_find_no_conflict(
+    lnurlp: RedirectPath, lnaddress: RedirectPath, nostrrelay: RedirectPath
+):
+    assert not nostrrelay.find_in_conflict(
+        [lnurlp, lnaddress]
     ), "Paths are not in conflict."
-
-    assert not nostrrelay_path_lnurl.in_conflict(
-        redirect_path_lnurl
-    ), "Paths are not in conflict (commutative)."
+    assert not lnurlp.find_in_conflict([nostrrelay]), "Paths are not in conflict."
+    assert not lnaddress.find_in_conflict([nostrrelay]), "Paths are not in conflict."
