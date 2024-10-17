5ad574736a1a3911cae4787292c3671b006321fe
Vlad Stan <stan.v.vlad@gmail.com>
2024-10-17 17:44:34 +0300

    fix: separate ALTER from SELECT

--- lnbits/core/migrations.py
+++ lnbits/core/migrations.py
@@ -595,6 +595,10 @@ async def m026_update_payment_table(db):
     await db.execute("ALTER TABLE apipayments ADD COLUMN extension TEXT")
     await db.execute("ALTER TABLE apipayments ADD COLUMN created_at TIMESTAMP")
     await db.execute("ALTER TABLE apipayments ADD COLUMN updated_at TIMESTAMP")
+    await db.execute("ALTER TABLE apipayments ADD COLUMN fiat_amounts TEXT")
+
+
+async def m027_update_apipayments_data(db):
     result = await db.execute("SELECT * FROM apipayments")
     payments = result.mappings().all()
     for payment in payments:
@@ -618,5 +622,5 @@ async def m026_update_payment_table(db):
         )
 
 
-async def m027_update_payment_table(db):
-    await db.execute("ALTER TABLE apipayments ADD COLUMN fiat_amounts TEXT")
+
+
