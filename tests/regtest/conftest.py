c41c5c087dc4b37ba617be58f4f59e6ecf8f2c68
dni ⚡ <office@dnilabs.com>
2024-12-11 07:06:35 +0100

    regtest anyio

--- tests/regtest/conftest.py
+++ tests/regtest/conftest.py
@@ -3,6 +3,11 @@ import pytest
 from .helpers import get_hold_invoice, get_real_invoice
 
 
+@pytest.fixture(scope="session")
+def anyio_backend():
+    return "asyncio"
+
+
 @pytest.fixture(scope="function")
 async def hold_invoice():
     invoice = get_hold_invoice(100)
