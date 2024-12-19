85b2f30a1aaaa06e8dc4fe085f10bad90a4f52d5
Arc <ben@arc.wales>
2024-12-19 13:38:25 +0000

    format

--- lnbits/static/js/wallet.js
+++ lnbits/static/js/wallet.js
@@ -727,8 +727,7 @@ window.app = Vue.createApp({
       this.$q.localStorage.set('lnbits.disclaimerShown', true)
     }
     // check blanace priority
-    if (
-      this.$q.localStorage.getItem('lnbits.isPrioritySwapped')) {
+    if (this.$q.localStorage.getItem('lnbits.isPrioritySwapped')) {
       this.isPrioritySwapped = this.$q.localStorage.getItem(
         'lnbits.isPrioritySwapped'
       )
