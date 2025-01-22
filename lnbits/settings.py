1917e7001dcf7b0e7a23a1650ece489ebe2a52ab
Tim Heide <40597235+timheide@users.noreply.github.com>
2025-01-22 22:33:28 +0100

    Update lnbits/settings.py
    Co-authored-by: Pavol Rusnak <pavol@rusnak.io>

--- lnbits/settings.py
+++ lnbits/settings.py
@@ -500,7 +500,7 @@ class BreezSdkFundingSource(LNbitsSettings):
     breez_greenlight_invite_code: Optional[str] = Field(default=None)
     breez_greenlight_device_key: Optional[str] = Field(default=None)
     breez_greenlight_device_cert: Optional[str] = Field(default=None)
-    breez_use_trampoline: bool = Field(default=False)
+    breez_use_trampoline: bool = Field(default=True)
 
 
 class BoltzFundingSource(LNbitsSettings):
