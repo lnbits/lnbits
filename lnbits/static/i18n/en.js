5babe61bafe3d147701d3fff7c549241b299d28c
Tiago Vasconcelos <talvasconcelos@gmail.com>
2025-01-23 17:33:41 +0000

    details page

--- lnbits/static/i18n/en.js
+++ lnbits/static/i18n/en.js
@@ -495,6 +495,5 @@ window.localisation.en = {
   payment_chart_flow: 'Monthly Payment Flow',
   payment_chart_status: 'Payment Status',
   payment_chart_tx_per_wallet: 'Transactions per Wallet',
-  payment_details_back: 'Back to Payments',
-  payment_chart_tags: 'Payments by Tags'
+  payment_details_back: 'Back to Payments'
 }
