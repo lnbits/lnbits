fa42414503a08066ec834930cf49efca4bfa7527
Vlad Stan <stan.v.vlad@gmail.com>
2025-01-30 09:59:33 +0200

    fix: convert BigDecimal object to int (postgres only)

--- lnbits/core/services/funding_source.py
+++ lnbits/core/services/funding_source.py
@@ -22,7 +22,7 @@ async def get_balance_delta() -> BalanceDelta:
     status = await funding_source.status()
     lnbits_balance = await get_total_balance()
     return BalanceDelta(
-        lnbits_balance_sats=lnbits_balance // 1000,
+        lnbits_balance_sats=int(lnbits_balance) // 1000,
         node_balance_sats=status.balance_msat // 1000,
     )
 
