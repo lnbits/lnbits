9dcf701d6bdeb77c24a91c6dab5796d984d0ce78
dni ⚡ <office@dnilabs.com>
2025-02-27 16:07:26 +0100

    rename username and password trigger, hardcoded pw warning

--- lnbits/settings.py
+++ lnbits/settings.py
@@ -582,7 +582,7 @@ class NodeUISettings(LNbitsSettings):
 
 class AuthMethods(Enum):
     user_id_only = "user-id-only"
-    username_and_password = "username-password"
+    username = "username-password"
     nostr_auth_nip98 = "nostr-auth-nip98"
     google_auth = "google-auth"
     github_auth = "github-auth"
@@ -592,7 +592,7 @@ class AuthMethods(Enum):
     def all(cls):
         return [
             AuthMethods.user_id_only.value,
-            AuthMethods.username_and_password.value,
+            AuthMethods.username.value,
             AuthMethods.nostr_auth_nip98.value,
             AuthMethods.google_auth.value,
             AuthMethods.github_auth.value,
@@ -606,7 +606,7 @@ class AuthSettings(LNbitsSettings):
     auth_allowed_methods: list[str] = Field(
         default=[
             AuthMethods.user_id_only.value,
-            AuthMethods.username_and_password.value,
+            AuthMethods.username.value,
         ]
     )
     # How many seconds after login the user is allowed to update its credentials.
