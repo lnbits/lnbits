46192148d4f91e47ae512b27df3100efe8908b63
arcbtc <ben@arc.wales>
2025-03-02 21:04:08 +0000

    Encourage protonmail use, because its great.

--- lnbits/settings.py
+++ lnbits/settings.py
@@ -397,7 +397,7 @@ class NotificationsSettings(LNbitsSettings):
     lnbits_email_notifications_enabled: bool = Field(default=False)
     lnbits_email_notifications_email: str = Field(default="")
     lnbits_email_notifications_password: str = Field(default="")
-    lnbits_email_notifications_server: str = Field(default="smtp.gmail.com")
+    lnbits_email_notifications_server: str = Field(default="smtp.protonmail.ch")
     lnbits_email_notifications_port: int = Field(default=587)
     lnbits_email_notifications_to_emails: list[str] = Field(default=[])
 
