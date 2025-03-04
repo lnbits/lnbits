481414e03d540275f04e44045a989d3682fdfffa
dni ⚡ <office@dnilabs.com>
2025-03-04 12:56:07 +0100

    revert email regex

--- lnbits/helpers.py
+++ lnbits/helpers.py
@@ -201,11 +201,7 @@ def generate_filter_params_openapi(model: Type[FilterModel], keep_optional=False
 
 
 def is_valid_email_address(email: str) -> bool:
-    # https://regexr.com/2rhq7
-    email_regex = (
-        r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@"
-        r"(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
-    )
+    email_regex = r"[A-Za-z0-9\._%+-]+@[A-Za-z0-9\.-]+\.[A-Za-z]{2,63}"
     return re.fullmatch(email_regex, email) is not None
 
 
