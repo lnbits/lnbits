dc67e34fbd94f5ee1589297a9dc5778e8423a850
dni ⚡ <office@dnilabs.com>
2025-05-05 10:42:07 +0200

    isSatDenomination

--- lnbits/static/js/base.js
+++ lnbits/static/js/base.js
@@ -495,6 +495,7 @@ window.windowMixin = {
         ? this.$q.localStorage.getItem('lnbits.backgroundImage')
         : USE_DEFAULT_BGIMAGE,
       isUserAuthorized: false,
+      isSatDenomination: WINDOW_SETTINGS['LNBITS_DENOMINATION'] == 'sats',
       walletEventListeners: [],
       backgroundImage: '',
       ...WINDOW_SETTINGS
