71cbfad8a1c8b3ff9f75a88342269f15033a25e8
dni ⚡ <office@dnilabs.com>
2025-05-11 06:04:00 +0200

    refactor: untangle lnd's macaroon encryption with AEScipher
    clean up the relation from lnd's load_macaroon function and
    utils/crypto.py AEScipher
    
    - rename some variables/function names in AEScipher class to be more
    descriptive

--- lnbits/wallets/macaroon/macaroon.py
+++ lnbits/wallets/macaroon/macaroon.py
@@ -1,45 +1,45 @@
 import base64
-
-from loguru import logger
+from getpass import getpass
+from typing import Optional
 
 from lnbits.utils.crypto import AESCipher
 
 
-def load_macaroon(macaroon: str) -> str:
-    """Returns hex version of a macaroon encoded in base64 or the file path.
+def load_macaroon(
+    macaroon: Optional[str] = None,
+    encrypted_macaroon: Optional[str] = None,
+) -> str:
+    """Returns hex version of a macaroon encoded in base64 or the file path."""
 
-    :param macaroon: Macaroon encoded in base64 or file path.
-    :type macaroon: str
-    :return: Hex version of macaroon.
-    :rtype: str
-    """
+    if macaroon is None and encrypted_macaroon is None:
+        raise ValueError("Either macaroon or encrypted_macaroon must be provided.")
+
+    if encrypted_macaroon:
+        # if the macaroon is encrypted, decrypt it
+        # and return the hex version
+        key = getpass("Enter the macaroon encryption key: ")
+        aes = AESCipher(key.encode())
+        return aes.decrypt(encrypted_macaroon).hex()
+
+    assert macaroon, "macaroon must be set here"
 
     # if the macaroon is a file path, load it and return hex version
     if macaroon.split(".")[-1] == "macaroon":
         with open(macaroon, "rb") as f:
             macaroon_bytes = f.read()
             return macaroon_bytes.hex()
-    else:
-        # if macaroon is a provided string
-        # check if it is hex, if so, return
-        try:
-            bytes.fromhex(macaroon)
-            return macaroon
-        except ValueError:
-            pass
-        # convert the bas64 macaroon to hex
-        try:
-            macaroon = base64.b64decode(macaroon).hex()
-        except Exception:
-            pass
-    return macaroon
 
+    # if macaroon is a provided string
+    # check if it is hex, if so, return
+    try:
+        bytes.fromhex(macaroon)
+        return macaroon
+    except ValueError:
+        pass
 
-# todo: move to its own (crypto.py) file
-# if this file is executed directly, ask for a macaroon and encrypt it
-if __name__ == "__main__":
-    macaroon = input("Enter macaroon: ")
-    macaroon = load_macaroon(macaroon)
-    macaroon = AESCipher(description="encryption").encrypt(macaroon.encode())
-    logger.info("Encrypted macaroon:")
-    logger.info(macaroon)
+    # convert the base64 macaroon to hex
+    try:
+        macaroon = base64.b64decode(macaroon).hex()
+        return macaroon
+    except Exception as exc:
+        raise ValueError("macaroon must be a hex string or a base64 string") from exc
