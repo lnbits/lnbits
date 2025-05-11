8d64f2c3ef51616ad0d633881fa8b5f6e34d1e30
dni ⚡ <office@dnilabs.com>
2025-05-11 10:26:49 +0200

    fixup!

--- tests/unit/test_crypto_aes.py
+++ tests/unit/test_crypto_aes.py
@@ -14,7 +14,7 @@ from lnbits.utils.crypto import AESCipher
 )
 async def test_aes_encrypt_decrypt(key):
     aes = AESCipher(key)
-    original_text = b"Hello, World!"
-    encrypted_text = aes.encrypt(original_text)
+    original_text = "Hello, World!"
+    encrypted_text = aes.encrypt(original_text.encode())
     decrypted_text = aes.decrypt(encrypted_text)
     assert original_text == decrypted_text
