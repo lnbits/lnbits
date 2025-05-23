eebb3106dcb6607e0af9fcc3ee654891e344001c
Vlad Stan <stan.v.vlad@gmail.com>
2025-05-23 18:27:05 +0300

    feat: add wallets table

--- lnbits/static/js/wallets.js
+++ lnbits/static/js/wallets.js
@@ -3,7 +3,50 @@ window.WalletsPageLogic = {
   data() {
     return {
       user: null,
-      tab: 'wallets'
+      tab: 'wallets',
+      wallets: [],
+      walletsTable: {
+        columns: [
+          {
+            name: 'name',
+            align: 'left',
+            label: 'Name',
+            field: 'name',
+            sortable: true
+          },
+          {
+            name: 'currency',
+            align: 'left',
+            label: 'Currency',
+            field: 'currency',
+            sortable: true
+          },
+          {
+            name: 'balance_msat',
+            align: 'left',
+            label: 'Balance',
+            field: 'balance_msat',
+            sortable: false
+          },
+          {
+            name: 'id',
+            align: 'left',
+            label: 'Id',
+            field: 'id',
+            sortable: true
+          }
+        ],
+        pagination: {
+          sortBy: 'updated_at',
+          rowsPerPage: 12,
+          page: 1,
+          descending: true,
+          rowsNumber: 10
+        },
+        search: null,
+        hideEmpty: true,
+        loading: false
+      }
     }
   },
   methods: {
@@ -31,17 +74,23 @@ window.WalletsPageLogic = {
       }
     },
 
-    async getUserWallets() {
+    async getUserWallets(props) {
       try {
+        this.walletsTable.loading = true
+        const params = LNbits.utils.prepareFilterQuery(this.walletsTable, props)
         const {data} = await LNbits.api.request(
           'GET',
-          '/api/v1/wallet/paginated',
+          `/api/v1/wallet/paginated?${params}`,
           null
         )
         console.log('### data', data)
+        console.log('### total', data.total)
         this.wallets = data.data
+        this.walletsTable.pagination.rowsNumber = data.total
       } catch (e) {
         LNbits.utils.notifyApiError(e)
+      } finally {
+        this.walletsTable.loading = false
       }
     },
 
