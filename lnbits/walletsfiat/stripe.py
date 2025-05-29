59c4a09f0c529cf1091018754075276ebdb64b5a
Vlad Stan <stan.v.vlad@gmail.com>
2025-05-29 11:28:48 +0300

    feat: basic functionality for `stripe`

---
+++ lnbits/walletsfiat/stripe.py
@@ -0,0 +1,160 @@
+import asyncio
+import json
+from datetime import datetime, timedelta, timezone
+from typing import AsyncGenerator, Optional
+
+import httpx
+from loguru import logger
+
+from lnbits.settings import settings
+
+from .base import (
+    FiatWallet,
+    InvoiceResponse,
+    PaymentFailedStatus,
+    PaymentPendingStatus,
+    PaymentResponse,
+    PaymentStatus,
+    PaymentSuccessStatus,
+    StatusResponse,
+)
+
+
+class StripeWallet(FiatWallet):
+    """https://docs.stripe.com/api"""
+
+    def __init__(self):
+        if not settings.stripe_endpoint:
+            raise ValueError("Cannot initialize StripeWallet: missing endpoint.")
+
+        if not settings.stripe_secret_key:
+            raise ValueError("Cannot initialize LNbitsWallet: missing secret key.")
+        self.endpoint = self.normalize_endpoint(settings.lnbits_endpoint)
+        self.headers = {
+            "Authorization": settings.stripe_secret_key,
+            "User-Agent": settings.user_agent,
+        }
+        self.client = httpx.AsyncClient(base_url=self.endpoint, headers=self.headers)
+
+    async def cleanup(self):
+        try:
+            await self.client.aclose()
+        except RuntimeError as e:
+            logger.warning(f"Error closing stripe wallet connection: {e}")
+
+    async def status(self) -> StatusResponse:
+        try:
+            r = await self.client.get(url="/v1/balance", timeout=15)
+            r.raise_for_status()
+            data = r.json()
+
+            available_balance = data.get("available", {}).get("amount", 0)
+            pending_balance = data.get("pending", {}).get("amount", 0)
+
+            # todo: handle currency vs msats
+            return StatusResponse(None, available_balance + pending_balance)
+        except json.JSONDecodeError:
+            return StatusResponse("Server error: 'invalid json response'", 0)
+        except Exception as exc:
+            logger.warning(exc)
+            return StatusResponse(f"Unable to connect to {self.endpoint}.", 0)
+
+    async def create_invoice(
+        self,
+        amount: int,
+        payment_hash: str,
+        currency: Optional[str] = None,
+        memo: Optional[str] = None,
+        **kwargs,
+    ) -> InvoiceResponse:
+        currency = str(
+            currency or settings.lnbits_default_accounting_currency or "usd"
+        ).lower()
+
+        data: dict = {
+            "mode": "payment",
+            "success_url": settings.stripe_success_url or "https://lnbits.com",
+            "metadata": {
+                "payment_hash": payment_hash,
+            },
+            "items": [
+                {
+                    "quantity": 1,
+                    "price_data": {
+                        "currency": currency,
+                        "unit_amount": amount,
+                        "product_data": {"name": memo or "LNbits Invoice"},
+                    },
+                }
+            ],
+        }
+
+        try:
+            r = await self.client.post(url="/v1/checkout/sessions", data=data)
+            r.raise_for_status()
+            data = r.json()
+
+            session_id = data.get("id")
+            if not session_id:
+                return InvoiceResponse(
+                    ok=False, error_message="Server error: 'missing session id'"
+                )
+            payment_request = data.get("url")
+            if not payment_request:
+                return InvoiceResponse(
+                    ok=False, error_message="Server error: 'missing payment URL'"
+                )
+
+            return InvoiceResponse(
+                ok=True, checking_id=session_id, payment_request=payment_request
+            )
+        except json.JSONDecodeError:
+            return InvoiceResponse(
+                ok=False, error_message="Server error: 'invalid json response'"
+            )
+        except Exception as exc:
+            logger.warning(exc)
+            return InvoiceResponse(
+                ok=False, error_message=f"Unable to connect to {self.endpoint}."
+            )
+
+    async def pay_invoice(self, payment_request: str) -> PaymentResponse:
+        raise NotImplementedError("Stripe does not support paying invoices directly.")
+
+    async def get_invoice_status(self, checking_id: str) -> PaymentStatus:
+        try:
+            r = await self.client.get(
+                url=f"/api/v1/payments/{checking_id}",
+            )
+            r.raise_for_status()
+
+            data = r.json()
+            payment_status = data.get("payment_status")
+            if not payment_status:
+                return PaymentPendingStatus()
+            if payment_status == "paid":
+                # todo: handle fee and preimage
+                return PaymentSuccessStatus()
+
+            expires_at = data.get("expires_at")
+            _24_hours_ago = datetime.now(timezone.utc) - timedelta(hours=24)
+            if expires_at and expires_at < _24_hours_ago:
+                # be defensive: add a 24 hour buffer
+                return PaymentFailedStatus()
+
+            return PaymentPendingStatus()
+        except Exception as exc:
+            logger.debug(f"Error getting invoice status: {exc}")
+            return PaymentPendingStatus()
+
+    async def get_payment_status(self, checking_id: str) -> PaymentStatus:
+        raise NotImplementedError("Stripe does not support outgoinf payments.")
+
+    async def paid_invoices_stream(self) -> AsyncGenerator[str, None]:
+        logger.warning(
+            "Stripe does not support paid invoices stream. Use webhooks instead."
+        )
+        mock_queue: asyncio.Queue[str] = asyncio.Queue(0)
+        while settings.lnbits_running:
+            value = await mock_queue.get()
+            yield value
