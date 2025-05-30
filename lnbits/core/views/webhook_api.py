a013b5f3383dca36efaecb223dbe46502cd5c6d8
Vlad Stan <stan.v.vlad@gmail.com>
2025-05-30 17:51:07 +0300

    feat: handle `stripe` notifications

--- lnbits/core/views/webhook_api.py
+++ lnbits/core/views/webhook_api.py
@@ -1,4 +1,7 @@
 from fastapi import APIRouter, Request
+from loguru import logger
+
+from lnbits.core.crud.payments import get_standalone_payment
 
 webhook_router = APIRouter(prefix="/api/v1/webhook", tags=["Webhooks"])
 
@@ -9,7 +12,30 @@ async def api_generic_webhook_handler(provider_name: str, request: Request):
     print(f"Headers: {request.headers}")
     body = await request.body()
     print(f"Body: {body.decode('utf-8')}")
+    if provider_name.lower() == "stripe":
+        event = await request.json()
+        await handle_stripe_event(event)
     return {
         "status": "success",
         "message": f"Webhook received successfully from '{provider_name}'.",
     }
+
+
+async def handle_stripe_event(event: dict):
+    event_id = event.get("id")
+    event_object = event.get("data", {}).get("object", {})
+    object_type = event_object.get("object")
+    payment_hash = event_object.get("metadata", {}).get("payment_hash")
+    logger.debug(
+        f"Handling Stripe event: '{event_id}'. Type: '{object_type}'."
+        f" Payment hash: '{payment_hash}'."
+    )
+    if not payment_hash:
+        logger.warning("Stripe event does not contain a payment hash.")
+        return
+
+    payment = await get_standalone_payment(payment_hash)
+    if not payment:
+        logger.warning(f"No payment found for hash: '{payment_hash}'.")
+        return
+    await payment.check_fiat_status()
