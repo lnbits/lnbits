6a4c9ca6fc2e55007bd504268a6dec836e8d9cba
dni ⚡ <office@dnilabs.com>
2025-06-11 11:43:25 +0200

    fixup tests

--- tests/regtest/conftest.py
+++ tests/regtest/conftest.py
@@ -27,5 +27,8 @@ async def real_amountless_invoice():
 @pytest.fixture(scope="function")
 async def real_invoice_noroute():
     invoice = get_real_invoice_noroute(100)
-    yield {"bolt11": invoice["payment_request"]}
+    yield {
+        "bolt11": invoice["payment_request"],
+        "payment_hash": invoice["r_hash"],
+    }
     del invoice
