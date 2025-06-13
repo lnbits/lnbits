893570e8bd4b979f6a787c7d5f94d76b6029cb8a
arcbtc <ben@arc.wales>
2025-06-13 20:47:43 +0100

    not needed

--- dockerboltz.sh
+++ dockerboltz.sh
@@ -7,14 +7,6 @@ boltzd --standalone &
 # Wait for boltzd to come up
 sleep 5
 
-# Create Liquid wallet (if needed), capture mnemonic
-echo "Creating Liquid wallet (lnbits)..."
-MNEMONIC=$(yes N | boltzcli wallet create lnbits lbtc 2>&1 | awk '/Mnemonic:/ {getline; print}')
-export BOLTZ_CLIENT_WALLET_MNEMONIC="$MNEMONIC"
-
-# Show the mnemonic (for debug)
-echo "Wallet mnemonic: $BOLTZ_CLIENT_WALLET_MNEMONIC"
-
 # Start LNbits
 echo "Starting LNbits..."
 exec poetry run lnbits --port $LNBITS_PORT --host $LNBITS_HOST --forwarded-allow-ips='*'
