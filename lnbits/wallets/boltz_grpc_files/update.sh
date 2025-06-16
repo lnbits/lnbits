bb810a364367a9c5bfb7437db4ed6bd9c4e115fa
Arc <ben@arc.wales>
2025-06-16 15:41:00 +0100

    Update bindings

--- lnbits/wallets/boltz_grpc_files/update.sh
+++ lnbits/wallets/boltz_grpc_files/update.sh
@@ -1,3 +1,3 @@
 
-wget https://raw.githubusercontent.com/BoltzExchange/boltz-client/master/boltzrpc/boltzrpc.proto -O lnbits/wallets/boltz_grpc_files/boltzrpc.proto
-poetry run python -m grpc_tools.protoc -I . --python_out=. --grpc_python_out=. --pyi_out=. lnbits/wallets/boltz_grpc_files/boltzrpc.proto
+wget https://raw.githubusercontent.com/BoltzExchange/boltz-client/refs/heads/master/pkg/boltzrpc/boltzrpc.proto -O boltz_grpc_files/boltzrpc.proto
+poetry run python -m grpc_tools.protoc -I . --python_out=. --grpc_python_out=. --pyi_out=. boltz_grpc_files/boltzrpc.proto
