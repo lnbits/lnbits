b0fdc7b28ccef3eb3a0c07f6d67020a52f86e4da
arcbtc <ben@arc.wales>
2025-06-16 16:51:42 +0100

    cheeky

--- lnbits/wallets/boltz_grpc_files/boltzrpc_pb2_grpc.py
+++ lnbits/wallets/boltz_grpc_files/boltzrpc_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-from boltz_grpc_files import boltzrpc_pb2 as boltz__grpc__files_dot_boltzrpc__pb2
+from . import boltzrpc_pb2 as boltz__grpc__files_dot_boltzrpc__pb2
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 
 GRPC_GENERATED_VERSION = '1.69.0'
