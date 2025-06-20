e236110e98a7fb55b94e4452585c8a7f5858f612
Vlad Stan <stan.v.vlad@gmail.com>
2025-06-20 21:00:39 +0300

    feat: check stripe signature

--- lnbits/core/views/callback_api.py
+++ lnbits/core/views/callback_api.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter, Request
 
 from lnbits.core.models.misc import SimpleStatus
-from lnbits.core.services.fiat_providers import handle_stripe_event
+from lnbits.core.services.fiat_providers import (
+    check_stripe_signature,
+    handle_stripe_event,
+)
+from lnbits.settings import settings
 
 callback_router = APIRouter(prefix="/api/v1/callback", tags=["callback"])
 
@@ -12,9 +16,16 @@ async def api_generic_webhook_handler(
 ) -> SimpleStatus:
     print("### Received webhook request ###", provider_name)
     print(f"Headers: {request.headers}")
+
     body = await request.body()
     print(f"Body: {body.decode('utf-8')}")
+
     if provider_name.lower() == "stripe":
+        payload = await request.body()
+        sig_header = request.headers.get("Stripe-Signature")
+        check_stripe_signature(
+            payload, sig_header, settings.stripe_webhook_signing_secret
+        )
         event = await request.json()
         await handle_stripe_event(event)
     return SimpleStatus(
