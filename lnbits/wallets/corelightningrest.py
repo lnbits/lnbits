a5f6e9d1b10e3fbd9d1eb08f73b88ec85fcbae35
Vlad Stan <stan.v.vlad@gmail.com>
2025-07-04 16:12:21 +0300

    fix: unsafe random

--- lnbits/wallets/corelightningrest.py
+++ lnbits/wallets/corelightningrest.py
@@ -1,7 +1,7 @@
 import asyncio
 import json
-import random
 from collections.abc import AsyncGenerator
+from secrets import token_urlsafe
 from typing import Optional
 
 import httpx
@@ -111,7 +111,7 @@ class CoreLightningRestWallet(Wallet):
         unhashed_description: Optional[bytes] = None,
         **kwargs,
     ) -> InvoiceResponse:
-        label = kwargs.get("label", f"lbl{random.random()}")
+        label = kwargs.get("label", f"lbl{token_urlsafe(16)}")
         data: dict = {
             "amount": amount * 1000,
             "description": memo,
