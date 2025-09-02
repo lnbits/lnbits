5021570f68aff5a601fcf04d0ae214fc06351f09
dni ⚡ <office@dnilabs.com>
2025-09-02 10:02:22 +0200

    feat: add `has_connection`, `listen` and `receive_queue` to websocket_manager (#3330)

--- lnbits/core/views/websocket_api.py
+++ lnbits/core/views/websocket_api.py
@@ -1,33 +1,20 @@
-from fastapi import (
-    APIRouter,
-    WebSocket,
-    WebSocketDisconnect,
-)
+from fastapi import APIRouter, WebSocket
 
-from lnbits.settings import settings
-
-from ..services import (
-    websocket_manager,
-    websocket_updater,
-)
+from ..services import websocket_manager
 
 websocket_router = APIRouter(prefix="/api/v1/ws", tags=["Websocket"])
 
 
 @websocket_router.websocket("/{item_id}")
-async def websocket_connect(websocket: WebSocket, item_id: str):
-    await websocket_manager.connect(websocket, item_id)
-    try:
-        while settings.lnbits_running:
-            await websocket.receive_text()
-    except WebSocketDisconnect:
-        websocket_manager.disconnect(websocket)
+async def websocket_connect(websocket: WebSocket, item_id: str) -> None:
+    conn = await websocket_manager.connect(item_id, websocket)
+    await websocket_manager.listen(conn)
 
 
 @websocket_router.post("/{item_id}")
 async def websocket_update_post(item_id: str, data: str):
     try:
-        await websocket_updater(item_id, data)
+        await websocket_manager.send(item_id, data)
         return {"sent": True, "data": data}
     except Exception:
         return {"sent": False, "data": data}
@@ -36,7 +23,7 @@ async def websocket_update_post(item_id: str, data: str):
 @websocket_router.get("/{item_id}/{data}")
 async def websocket_update_get(item_id: str, data: str):
     try:
-        await websocket_updater(item_id, data)
+        await websocket_manager.send(item_id, data)
         return {"sent": True, "data": data}
     except Exception:
         return {"sent": False, "data": data}
