130e7f8c7d93e308edc234595a2929d1fb087b8d
Weston Keele <wekeele@proton.me>
2025-09-13 23:30:00 +0200

    nix: Re-enable and fix basic nix flake check for nix package and module

--- nix/tests/nixos-module/default.nix
+++ nix/tests/nixos-module/default.nix
@@ -1,16 +1,19 @@
-{ pkgs, makeTest, inputs }:
-makeTest {
+{ pkgs, flake }:
+pkgs.nixosTest {
   name = "lnbits-nixos-module";
   nodes = {
     client = { config, pkgs, ... }: {
       environment.systemPackages = [ pkgs.curl ];
     };
     lnbits = { ... }: {
-      imports = [ inputs.self.nixosModules.default ];
+      imports = [ flake.nixosModules.${pkgs.system}.default ];
       services.lnbits = {
         enable = true;
         openFirewall = true;
         host = "0.0.0.0";
+        env = {
+          LNBITS_ADMIN_UI = "false";
+        };
       };
     };
   };
@@ -19,8 +22,10 @@ makeTest {
     lnbits.wait_for_open_port(${toString nodes.lnbits.config.services.lnbits.port})
     client.wait_for_unit("multi-user.target")
     with subtest("Check that the lnbits webserver can be reached."):
-        assert "<title>LNbits</title>" in client.succeed(
-            "curl -sSf http:/lnbits:8231/ | grep title"
+        output = client.succeed(
+            "curl -sSf http://lnbits:8231/ | grep title | head -n1"
         )
+
+        assert "<title>LNbits</title>" in output;
   '';
 }
