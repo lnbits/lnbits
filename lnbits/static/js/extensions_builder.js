78846ad68311790af3cad6245802cbe01604e29e
Vlad Stan <stan.v.vlad@gmail.com>
2025-09-15 16:27:19 +0300

    feat: basic UI

--- lnbits/static/js/extensions_builder.js
+++ lnbits/static/js/extensions_builder.js
@@ -1,66 +1,67 @@
 window.ExtensionsBuilderPageLogic = {
   data: function () {
     return {
-      step: 0,
+      step: 5,
+      extensionData: {
+        id: '',
+        name: '',
+        short_description: '',
+        description: '',
+        generate: {
+          settings: true,
+          owner: true,
+          customer: true
+        },
+        public_page: {
+          has_public_page: true,
+          owner_data_fields: {
+            name: '',
+            description: ''
+          },
+          client_data_fields: {
+            public_inputs: []
+          },
+          action_fields: {
+            generate_action: true,
+            wallet_id: '',
+            currency: '',
+            amount: ''
+          }
+        },
+
+        settingsType: 'user',
+        settingsFields: [],
+        ownerFields: [],
+        client_fields: []
+      },
+
       settingsTypes: [
         {label: 'User Settings', value: 'user'},
         {label: 'Admin Settings', value: 'admin'}
-      ],
-      generate: {
-        settings: true,
-        owner: true,
-        customer: true,
-        publicPages: true
-      },
-
-      settingsType: 'user',
-      settingsFields: [],
-      ownerFields: [
-        {
-          name: 'created_at',
-          type: 'datetime',
-          label: 'Created At',
-          hint: 'The date and time when the record was created.',
-          optional: false,
-          sortable: true,
-          editable: false,
-          searchable: true,
-          readonly: true,
-          fields: []
-        },
-        {
-          name: 'updated_at',
-          type: 'datetime',
-          label: 'Updated At',
-          hint: 'The date and time when the record was last updated.',
-          optional: false,
-          sortable: true,
-          searchable: true,
-          editable: false,
-          readonly: true,
-          fields: []
-        },
-
-        {
-          name: 'extra',
-          type: 'json',
-          label: 'Extra Data',
-          hint: 'This data is not searchable. But adding data fields here does not require a database migration.',
-          optional: false,
-          sortable: true,
-          searchable: true,
-          editable: false,
-          readonly: true,
-          fields: [] // For nested fields in JSON type
-        }
-      ],
-      customerFields: []
+      ]
     }
   },
 
-  methods: {},
+  methods: {
+    nextStep() {
+      this.$q.localStorage.set(
+        'lnbits.extension.builder.data',
+        JSON.stringify(this.extensionData)
+      )
+      this.$refs.stepper.next()
+      console.log("### Next step", JSON.stringify(this.extensionData))
+    },
+    previousStep() {
+      this.$refs.stepper.previous()
+    }
+  },
   created: function () {
-    this.customerFields = JSON.parse(JSON.stringify(this.ownerFields))
+    const extensionData = this.$q.localStorage.getItem(
+      'lnbits.extension.builder.data'
+    )
+    if (extensionData) {
+      this.extensionData = JSON.parse(extensionData)
+    }
   },
   mixins: [windowMixin]
 }
