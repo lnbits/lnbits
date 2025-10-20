54bce2e1e048055966f469f25b252b8c897a42d1
Vlad Stan <stan.v.vlad@gmail.com>
2025-10-20 09:55:24 +0300

    fix: exclude deleted wallets bu default

--- lnbits/core/crud/wallets.py
+++ lnbits/core/crud/wallets.py
@@ -104,7 +104,7 @@ async def delete_unused_wallets(
 
 
 async def get_wallet(
-    wallet_id: str, deleted: bool | None = None, conn: Connection | None = None
+    wallet_id: str, deleted: bool | None = False, conn: Connection | None = None
 ) -> Wallet | None:
     query = """
             SELECT *, COALESCE((
