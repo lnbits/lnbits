4fdc87dd7fd210a5f26cbc84b392d939ebd1ded0
dni ⚡ <office@dnilabs.com>
2025-11-10 14:40:34 +0100

    get rid of dynamic component

--- lnbits/static/js/init-app.js
+++ lnbits/static/js/init-app.js
@@ -1,124 +1,3 @@
-const DynamicComponent = {
-  props: {
-    fetchUrl: {
-      type: String,
-      required: true
-    },
-    scripts: {
-      type: Array,
-      default: () => []
-    }
-  },
-  data() {
-    return {
-      keys: []
-    }
-  },
-  async mounted() {
-    await this.loadDynamicContent()
-  },
-  methods: {
-    async loadScript(src) {
-      return new Promise((resolve, reject) => {
-        const existingScript = document.querySelector(`script[src="${src}"]`)
-        if (existingScript) {
-          existingScript.remove()
-        }
-        const script = document.createElement('script')
-        script.src = src
-        script.async = true
-        script.onload = resolve
-        script.onerror = () =>
-          reject(new Error(`Failed to load script: ${src}`))
-        document.head.appendChild(script)
-      })
-    },
-    async loadDynamicContent() {
-      this.$q.loading.show()
-      try {
-        const cleanUrl = this.fetchUrl.split('#')[0]
-        //grab page content, need to be before loading scripts
-        const response = await fetch(cleanUrl, {
-          credentials: 'include',
-          headers: {
-            Accept: 'text/html',
-            'X-Requested-With': 'XMLHttpRequest'
-          }
-        })
-
-        const html = await response.text()
-
-        // load window variables
-        const parser = new DOMParser()
-        const htmlDocument = parser.parseFromString(html, 'text/html')
-        const inlineScript = htmlDocument.querySelector('#window-vars-script')
-        if (inlineScript) {
-          new Function(inlineScript.innerHTML)() // Execute the script
-        }
-
-        //load scripts defined in the route
-        await this.loadScript('/static/js/base.js')
-        for (const script of this.scripts) {
-          await this.loadScript(script)
-        }
-
-        //housecleaning, remove old component
-        const previousRouteName =
-          this.$router.currentRoute.value.meta.previousRouteName
-        if (
-          previousRouteName &&
-          window.app._context.components[previousRouteName]
-        ) {
-          delete window.app._context.components[previousRouteName]
-        }
-        //load component logic
-        const logicKey = `${this.$route.name}PageLogic`
-        const componentLogic = window[logicKey]
-
-        if (!componentLogic) {
-          throw new Error(
-            `Component logic '${logicKey}' not found. Ensure it is defined in the script.`
-          )
-        }
-
-        //Add mixins
-        componentLogic.mixins = componentLogic.mixins || []
-        if (window.windowMixin) {
-          componentLogic.mixins.push(window.windowMixin)
-        }
-
-        //Build component
-        window.app.component(this.$route.name, {
-          ...componentLogic,
-          template: html // Use the fetched HTML as the template
-        })
-        delete window[logicKey] //dont need this anymore
-        this.$forceUpdate()
-      } catch (error) {
-        console.error('Error loading dynamic content:', error)
-      } finally {
-        this.$q.loading.hide()
-      }
-    }
-  },
-  watch: {
-    $route(to, from) {
-      const validRouteNames = routes.map(route => route.name)
-      if (validRouteNames.includes(to.name)) {
-        this.$router.currentRoute.value.meta.previousRouteName = from.name
-        this.loadDynamicContent()
-      } else {
-        console.log(
-          `Route '${to.name}' is not valid. Leave this one to Fastapi.`
-        )
-      }
-    }
-  },
-  template: `
-      <component :is="$route.name"></component>
-  `
-}
-
 const routes = [
   {
     path: '/node',
@@ -192,17 +71,6 @@ window.router = VueRouter.createRouter({
   routes
 })
 
-window.app.mixin({
-  computed: {
-    isVueRoute() {
-      const currentPath = window.location.pathname
-      const matchedRoute = window.router.resolve(currentPath)
-      const isVueRoute = matchedRoute?.matched?.length > 0
-      return isVueRoute
-    }
-  }
-})
-
 window.app.use(VueQrcodeReader)
 window.app.use(Quasar, {
   config: {
@@ -214,7 +82,6 @@ window.app.use(Quasar, {
 window.app.use(window.i18n)
 window.app.provide('g', g)
 window.app.use(window.router)
-window.app.component('DynamicComponent', DynamicComponent)
 window.app.mount('#vue')
 
 if (navigator.serviceWorker != null) {
