14d1fc1efe51137ef4fb1a345731959863d8ed53
dni ⚡ <office@dnilabs.com>
2025-11-12 12:13:00 +0100

    ext

--- lnbits/static/js/components.js
+++ lnbits/static/js/components.js
@@ -43,62 +43,6 @@ window.app.component('lnbits-wallet-list', {
   }
 })
 
-window.app.component('lnbits-extension-list', {
-  mixins: [window.windowMixin],
-  template: '#lnbits-extension-list',
-  data() {
-    return {
-      extensions: [],
-      searchTerm: ''
-    }
-  },
-  watch: {
-    'g.user.extensions': {
-      handler(newExtensions) {
-        this.loadExtensions()
-      },
-      deep: true
-    }
-  },
-  computed: {
-    userExtensions() {
-      return this.updateUserExtensions(this.searchTerm)
-    }
-  },
-  methods: {
-    async loadExtensions() {
-      try {
-        const {data} = await LNbits.api.request('GET', '/api/v1/extension')
-        this.extensions = data
-          .map(extension => LNbits.map.extension(extension))
-          .sort((a, b) => a.name.localeCompare(b.name))
-      } catch (error) {
-        LNbits.utils.notifyApiError(error)
-      }
-    },
-    updateUserExtensions(filterBy) {
-      const path = window.location.pathname
-      const userExtensions = this.g.user.extensions
-
-      return this.extensions
-        .filter(o => userExtensions.includes(o.code))
-        .filter(o => {
-          if (!filterBy) return true
-          return `${o.code} ${o.name} ${o.short_description} ${o.url}`
-            .toLocaleLowerCase()
-            .includes(filterBy.toLocaleLowerCase())
-        })
-        .map(obj => {
-          obj.isActive = path.startsWith(obj.url)
-          return obj
-        })
-    }
-  },
-  async created() {
-    await this.loadExtensions()
-  }
-})
-
 window.app.component('lnbits-manage', {
   mixins: [window.windowMixin],
   template: '#lnbits-manage',
--- lnbits/templates/components.vue
+++ lnbits/templates/components.vue
@@ -14,7 +14,8 @@ include('components/new_user_wallet.vue') %} {%
 include('components/lnbits-footer.vue') %} {%
 include('components/lnbits-header.vue') %} {%
 include('components/lnbits-drawer.vue') %} {%
-include('components/lnbits-home-logos.vue') %}
+include('components/lnbits-home-logos.vue') %} {%
+include('components/lnbits-extension-list.vue') %}
 
 <template id="lnbits-wallet-list">
   <q-list
@@ -104,51 +105,6 @@ include('components/lnbits-home-logos.vue') %}
         </q-item-section>
       </q-item-section>
     </q-item>
-  </q-list>
-</template>
-
-<template id="lnbits-extension-list">
-  <q-list
-    v-if="
-      (g.user && userExtensions && userExtensions.length > 0) || !!searchTerm
-    "
-    dense
-    class="lnbits-drawer__q-list"
-  >
-    <q-item>
-      <q-item-section>
-        <q-input
-          v-model="searchTerm"
-          dense
-          borderless
-          :label="$t('extensions')"
-        >
-        </q-input>
-      </q-item-section>
-    </q-item>
-    <q-item
-      v-for="extension in userExtensions"
-      :key="extension.code"
-      clickable
-      :active="extension.isActive"
-      tag="a"
-      :href="extension.url"
-    >
-      <q-item-section side>
-        <q-avatar size="md">
-          <q-img :src="extension.tile" style="max-width: 20px"></q-img>
-        </q-avatar>
-      </q-item-section>
-      <q-item-section>
-        <q-item-label lines="1"
-          ><span v-text="extension.name"></span>
-        </q-item-label>
-      </q-item-section>
-      <q-item-section side v-show="extension.isActive">
-        <q-icon name="chevron_right" color="grey-5" size="md"></q-icon>
-      </q-item-section>
-    </q-item>
-    <div class="lt-md q-mt-xl q-mb-xl"></div>
   </q-list>
 </template>
 
