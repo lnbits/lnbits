129a6140fe666490fb03fd0284f67e2ca7698250
dni ⚡ <office@dnilabs.com>
2026-01-30 10:46:03 +0100

    add to env example

--- lnbits/utils/logger.py
+++ lnbits/utils/logger.py
@@ -15,11 +15,11 @@ from lnbits.settings import settings
 def log_server_info():
     logger.info("LNbits Info")
     if settings.first_install:
-        logger.info("This is a fresh install of LNbits.")
+        logger.success("This is a fresh install of LNbits.")
         if settings.first_install_token:
-            logger.info(
-                f"FIRST_INSTALL_TOKEN: {settings.first_install_token}. "
-                "Please provide this token when creating a user on /first_install."
+            logger.success(
+                f"FIRST_INSTALL_TOKEN: `{settings.first_install_token}`. "
+                "Please provide this token on /first_install."
             )
     logger.info(f"Version: {settings.version}")
     logger.info(f"Baseurl: {settings.lnbits_baseurl}")
