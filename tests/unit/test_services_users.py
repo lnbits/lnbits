db2829a5f6d683c163d10276933bd1261f72e16c
Vlad Stan <stan.v.vlad@gmail.com>
2026-03-09 13:44:19 +0200

    chore: postpone test

--- tests/unit/test_services_users.py
+++ tests/unit/test_services_users.py
@@ -1,6 +1,7 @@
 from uuid import uuid4
 
 import pytest
+from sqlalchemy.exc import DBAPIError
 
 from lnbits.core.crud import (
     create_account,
@@ -13,6 +14,7 @@ from lnbits.core.crud import (
     get_wallets,
 )
 from lnbits.core.crud.settings import get_settings_field, set_settings_field
+from lnbits.db import DB_TYPE, POSTGRES
 from lnbits.core.models import Account
 from lnbits.core.models.extensions import UserExtension
 from lnbits.core.models.users import RegisterUser
@@ -100,7 +102,6 @@ async def test_create_user_account_no_check_creates_wallet_and_extensions(
     assert wallets[0].name == "Primary"
     assert {ext.extension for ext in user_extensions} == {"default-ext", "extra-ext"}
     assert all(ext.active is True for ext in user_extensions)
-
 
 @pytest.mark.anyio
 async def test_create_user_account_no_check_ignores_duplicate_extension_insert(
