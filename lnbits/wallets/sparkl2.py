2c66fb9be9fbf55fd196bf5b298ef91504b70c71
BrunswickBTC <saqupjfmonxwjbh@protonmail.com>
2026-04-29 14:09:03 -0500

    Fix interaction between lnbits sparkl2 wallet backend and the spark l2 sidecar app

--- lnbits/wallets/sparkl2.py
+++ lnbits/wallets/sparkl2.py
@@ -159,7 +159,8 @@ class SparkL2Wallet(Wallet):
                 "payment_hash": payment_hash,
             }
             res = await self._request("POST", "/v1/payments", payload)
-            checking_id = payment_hash or res.get("checking_id")
+            #checking_id = payment_hash or res.get("checking_id")
+            checking_id = res.get("checking_id")
             if not checking_id:
                 return PaymentResponse(
                     ok=False,
