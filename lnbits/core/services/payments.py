90237a1f525bc05ba418937f15d088e7a1df288a
Vlad Stan <stan.v.vlad@gmail.com>
2026-06-17 15:55:00 +0300

    doc: add some comments

--- lnbits/core/services/payments.py
+++ lnbits/core/services/payments.py
@@ -778,10 +778,12 @@ async def _pay_internal_invoice(
     await update_payment(internal_payment, conn=conn)
     logger.success(f"internal payment successful {internal_payment.checking_id}")
 
-    await _send_payment_notification_in_background(wallet.id, payment, conn=conn)
+    await _send_payment_notification_in_background(
+        wallet.id, payment, conn=conn
+    )  # notify the sender
     await _send_payment_notification_in_background(
         internal_payment.wallet_id, internal_payment, conn=conn
-    )
+    )  # notify the receiver
 
     # notify receiver asynchronously (extension listeners)
     from lnbits.tasks import internal_invoice_queue
