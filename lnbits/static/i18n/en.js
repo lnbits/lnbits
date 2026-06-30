63050dbbbd73d1ed8fb7b370fb03d488b592d44b
Vlad Stan <stan.v.vlad@gmail.com>
2026-06-30 11:20:02 +0300

    fix: i18n

--- lnbits/static/i18n/en.js
+++ lnbits/static/i18n/en.js
@@ -527,7 +527,10 @@ window.localisation.en = {
   extension_permission_ext_storage_write: 'Write extension storage',
   extension_permission_payments_watch: 'Watch payments',
   extension_permission_wallet_create_invoice: 'Create invoices',
-  extension_permission_wallet_create_invoice_public: 'Create public invoices',
+  extension_permission_wallet_create_invoice_public:
+    'Create incoming Lightning invoices for selected wallets',
+  extension_permission_wallet_create_invoice_public_desc:
+    'Create incoming Lightning invoices for selected wallets.',
   extension_permission_wallet_list: 'List wallets',
   create_extension: 'Create Extension',
   release_details_error: 'Cannot get the release details.',
