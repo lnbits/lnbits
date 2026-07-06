d157c43ebb4664b659a00b8c9c7c11e7c3bd6d05
dni <office@dnilabs.com>
2026-07-06 16:40:37 +0200

    remove old files

--- lnbits/core/models/extensions.py
+++ lnbits/core/models/extensions.py
@@ -456,6 +456,7 @@ class InstallableExtension(BaseModel):
 
         shutil.rmtree(self.ext_dir, True)
         shutil.copytree(Path(self.ext_upgrade_dir), Path(self.ext_dir))
+        shutil.rmtree(self.ext_upgrade_dir, True)
         logger.info(f"Extension {self.name} ({self.installed_version}) extracted.")
 
     def clean_extension_files(self):
