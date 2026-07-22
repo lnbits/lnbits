47c410c7210daa1f0ffbc517cfafbe8a7b7f5e50
Arc <ben@arc.wales>
2026-07-22 12:15:58 +0100

    54d8a098a audio, fullscreen, cursor

--- lnbits/static/js/wasm-extension-component.js
+++ lnbits/static/js/wasm-extension-component.js
@@ -658,6 +658,7 @@ window.WasmExtensionComponent = {
     },
     async requestBackgroundPaymentPermission(message) {
       const response = await this.requestExtensionPermissions({
+        forcePrompt: message?.forcePrompt === true,
         permissions: [
           {
             id: 'wallet.pay_invoice_background',
@@ -688,6 +689,7 @@ window.WasmExtensionComponent = {
       if (!permissions.length) {
         throw new Error('No permissions requested.')
       }
+      const forcePrompt = message.forcePrompt === true
 
       const requestedPermissions = permissions.map(permission =>
         this.normalizePermissionRequest(permission)
@@ -710,7 +712,7 @@ window.WasmExtensionComponent = {
           throw new Error('Permission check response did not match request.')
         }
 
-        if (check.approved) {
+        if (check.approved && !forcePrompt) {
           approvedLabels.push(permission.label)
           results.push({
             id: permission.id,
