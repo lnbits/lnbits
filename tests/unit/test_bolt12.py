13a5904be4bc699c586db5d3497727f13845881a
Vlad Stan <stan.v.vlad@gmail.com>
2026-09-16 14:52:39 +0300

    refactor: move bolt12 logic to utils

--- tests/unit/test_bolt12.py
+++ tests/unit/test_bolt12.py
@@ -5,11 +5,6 @@ import pytest
 from lnbits.core.crud import create_wallet, get_standalone_payment, get_wallet
 from lnbits.core.models import PaymentState, ValidatedPaymentRequest
 from lnbits.core.services import create_user_account, pay_invoice, pay_offer
-from lnbits.core.services.bolt12 import (
-    is_bolt12_offer,
-    looks_like_bolt12_offer,
-    parse_bolt12_offer,
-)
 from lnbits.core.services.payments import (
     _validate_offer_payment_request,
     _validate_payment_request,
@@ -17,6 +12,11 @@ from lnbits.core.services.payments import (
 )
 from lnbits.exceptions import PaymentError
 from lnbits.settings import Settings
+from lnbits.utils.bolt12 import (
+    is_bolt12_offer,
+    looks_like_bolt12_offer,
+    parse_bolt12_offer,
+)
 from lnbits.wallets.base import Feature
 from lnbits.wallets.fake import FakeWallet
 
